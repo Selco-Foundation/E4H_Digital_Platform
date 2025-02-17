63e949378c274780fba3255f822872d5c7bf2a42
Joa-beehyv <jeffrey.adams@beehyv.com>
2025-02-17 14:11:14 +0530

    feature-56: fix (#226)

--- backend/municipal-services/im-services/src/main/java/org/egov/im/web/models/storage/StorageResponse.java
+++ backend/municipal-services/im-services/src/main/java/org/egov/im/web/models/storage/StorageResponse.java
@@ -2,12 +2,14 @@ package org.egov.im.web.models.storage;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
-import java.io.File;
 import java.util.List;
+
 
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 public class StorageResponse {
     private List<File> files;
 }
