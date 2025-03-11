f826655c315f2076e32139147e61409eb7514053
Joa-beehyv <jeffrey.adams@beehyv.com>
2025-03-12 00:41:58 +0530

    feature-187: bug fixes and enhancements (#312)
    Added Processing Async simulatanoulsy in a single thread

---
+++ backend/municipal-services/im-services/src/main/java/org/egov/im/config/AsyncConfig.java
@@ -0,0 +1,25 @@
+package org.egov.im.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.AsyncConfigurer;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.ThreadPoolExecutor;
+
+@Configuration
+public class AsyncConfig implements AsyncConfigurer {
+
+    @Override
+    public Executor getAsyncExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(1);
+        executor.setMaxPoolSize(1);
+        executor.setQueueCapacity(1);
+        executor.setThreadNamePrefix("AsyncExecutor-");
+        executor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
+        executor.initialize();
+        return executor;
+    }
+}
