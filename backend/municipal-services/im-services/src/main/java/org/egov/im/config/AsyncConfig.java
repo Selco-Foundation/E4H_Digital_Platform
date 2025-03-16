ea8827e7cc284a354fa40f155f833c0b392621f0
Joa-beehyv <jeffrey.adams@beehyv.com>
2025-03-16 22:48:18 +0530

    Update AsyncConfig (#350)
    updating available processors to 2

--- backend/municipal-services/im-services/src/main/java/org/egov/im/config/AsyncConfig.java
+++ backend/municipal-services/im-services/src/main/java/org/egov/im/config/AsyncConfig.java
@@ -17,7 +17,7 @@ public class AsyncConfig implements AsyncConfigurer {
     public Executor getAsyncExecutor() {
         ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
 
-        int availableProcessors = 1;
+        int availableProcessors = 2;
         log.info("Available processors: {}", availableProcessors);
 
         executor.setCorePoolSize(availableProcessors);
