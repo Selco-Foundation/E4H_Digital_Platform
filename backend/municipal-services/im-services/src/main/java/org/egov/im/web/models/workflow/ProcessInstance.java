bb14626a3d0b4812d658a4e8361e9616a705d5fa
Sahil Vartak <85557319+sahilvartak-bot@users.noreply.github.com>
2025-04-09 19:59:31 +0530

    Remove unwanted notification action (#470)
    Co-authored-by: sahil.dileep <sahil.dileep@beehyv.com>

--- backend/municipal-services/im-services/src/main/java/org/egov/im/web/models/workflow/ProcessInstance.java
+++ backend/municipal-services/im-services/src/main/java/org/egov/im/web/models/workflow/ProcessInstance.java
@@ -72,8 +72,6 @@ public class ProcessInstance {
     @JsonProperty("state")
     private State state;
 
-    private String notificationAction;
-
     @Size(max=1024)
     @JsonProperty("comment")
     @Pattern(regexp = "^[a-zA-Z0-9!@#.,/:; ()&']*$")
