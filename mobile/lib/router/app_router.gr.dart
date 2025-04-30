5846c71f48d0c1ca42bb7a6868999a3026af111d
jogapps <jogappsdeveloper@gmail.com>
2025-04-30 08:52:15 +0100

    added inbox screen

--- mobile/lib/router/app_router.gr.dart
+++ mobile/lib/router/app_router.gr.dart
@@ -63,6 +63,12 @@ abstract class _$AppRouter extends RootStackRouter {
         child: const HomePage(),
       );
     },
+    InboxRoute.name: (routeData) {
+      return AutoRoutePage<dynamic>(
+        routeData: routeData,
+        child: const InboxPage(),
+      );
+    },
     InstallationReportRoute.name: (routeData) {
       return AutoRoutePage<dynamic>(
         routeData: routeData,
@@ -234,6 +240,20 @@ class HomeRoute extends PageRouteInfo<void> {
         );
 
   static const String name = 'HomeRoute';
+
+  static const PageInfo<void> page = PageInfo<void>(name);
+}
+
+/// generated route for
+/// [InboxPage]
+class InboxRoute extends PageRouteInfo<void> {
+  const InboxRoute({List<PageRouteInfo>? children})
+      : super(
+          InboxRoute.name,
+          initialChildren: children,
+        );
+
+  static const String name = 'InboxRoute';
 
   static const PageInfo<void> page = PageInfo<void>(name);
 }
