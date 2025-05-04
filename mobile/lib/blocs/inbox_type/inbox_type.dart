ed3e986463b37c5685ac605885deeb331f15b278
jogapps <jogappsdeveloper@gmail.com>
2025-05-04 21:19:36 +0100

    completed inbox screens

---
+++ mobile/lib/blocs/inbox_type/inbox_type.dart
@@ -0,0 +1,44 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'inbox_type.freezed.dart';
+
+// states
+@freezed
+class InboxTypeState with _$InboxTypeState {
+  const factory InboxTypeState.submitted() = InboxTypeSubmitted;
+  const factory InboxTypeState.rejected() = InboxTypeRejected;
+  const factory InboxTypeState.approved() = InboxTypeApproved;
+}
+
+// events
+@freezed
+class InboxTypeEvent with _$InboxTypeEvent {
+  const factory InboxTypeEvent.typeSelected(int inboxType) = InboxTypeSelected;
+}
+
+// bloc
+class InboxTypeBloc extends Bloc<InboxTypeEvent, InboxTypeState> {
+  InboxTypeBloc() : super(const InboxTypeState.submitted()) {
+    on<InboxTypeSelected>(_onTypeSelected);
+  }
+
+  Future<void> _onTypeSelected(
+    InboxTypeSelected event,
+    Emitter<InboxTypeState> emit,
+  ) async {
+    switch (event.inboxType) {
+      case 0:
+        emit(const InboxTypeState.submitted());
+        break;
+      case 1:
+        emit(const InboxTypeState.rejected());
+        break;
+      case 2:
+        emit(const InboxTypeState.approved());
+        break;
+      default:
+        emit(const InboxTypeState.submitted());
+    }
+  }
+}
