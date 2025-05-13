7f20ac597ca7d185190e6dceb930584b75e09c23
subhashini-egov <107110442+subhashini-egov@users.noreply.github.com>
2025-05-13 15:44:56 +0530

    Fix for vendor build failure (#665)

--- backend/e4h-services/vendor-registry/src/test/java/org/egov/web/controllers/OrganisationApiControllerTest.java
+++ backend/e4h-services/vendor-registry/src/test/java/org/egov/web/controllers/OrganisationApiControllerTest.java
@@ -20,6 +20,7 @@ import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -57,6 +58,9 @@ import org.junit.jupiter.api.Test;
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = OrganizationMain.class, properties = "spring.main.lazy-initialization=true")
 @AutoConfigureMockMvc
+@EnableAutoConfiguration(exclude = {
+    io.opentelemetry.instrumentation.spring.autoconfigure.OpenTelemetryAutoConfiguration.class
+})
 class OrganisationApiControllerTest {
 
     @Autowired
