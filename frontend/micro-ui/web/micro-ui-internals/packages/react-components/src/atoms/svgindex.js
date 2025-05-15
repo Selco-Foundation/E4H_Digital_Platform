d810333b661bc70fc5d9d3b81a735a3680a213ab
Dhruv <dhruv.g@beehyv.com>
2025-05-15 20:43:08 +0530

    #436, #437 Bug Fixes

--- frontend/micro-ui/web/micro-ui-internals/packages/react-components/src/atoms/svgindex.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/react-components/src/atoms/svgindex.js
@@ -1927,14 +1927,14 @@ const SkipForwardIcon = () => (
   </svg>
 );
 
-const PlayIcon = () => (
-  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 92.2 122.88" width="24" height="24" fill="currentColor">
+const PlayIcon = ({ color }) => (
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 92.2 122.88" width="24" height="24" fill={color}>
     <polygon points="92.2,60.97 0,122.88 0,0 92.2,60.97" />
   </svg>
 );
 
-const PauseIcon = () => (
-  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 87.72 122.88" width="24" height="24" fill="currentColor">
+const PauseIcon = ({ color }) => (
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 87.72 122.88" width="24" height="24" fill={color}>
     <path d="M0,0h35.54v122.88l-35.54,0V0L0,0z M52.18,0h35.54v122.88l-35.54,0V0L52.18,0z" />
   </svg>
 );
