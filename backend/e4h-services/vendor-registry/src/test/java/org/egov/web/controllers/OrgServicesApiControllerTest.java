35a286da0cc4e59967435c8f0be4e36e0bea435e
subhashini-egov <107110442+subhashini-egov@users.noreply.github.com>
2025-05-15 15:28:39 +0530

    Update OrgServicesApiControllerTest.java

--- backend/e4h-services/vendor-registry/src/test/java/org/egov/web/controllers/OrgServicesApiControllerTest.java
+++ backend/e4h-services/vendor-registry/src/test/java/org/egov/web/controllers/OrgServicesApiControllerTest.java
@@ -8,6 +8,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
