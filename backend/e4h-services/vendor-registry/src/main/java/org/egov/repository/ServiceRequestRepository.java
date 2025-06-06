de2b55209c820c9094ea334cacd177d7023163ed
Vineet <vineet.kumar@beehyv.com>
2025-06-06 15:01:03 +0530

    Fixed vendor registry issues.

--- backend/e4h-services/vendor-registry/src/main/java/org/egov/repository/ServiceRequestRepository.java
+++ backend/e4h-services/vendor-registry/src/main/java/org/egov/repository/ServiceRequestRepository.java
@@ -1,45 +1,23 @@
 package org.egov.repository;
 
-import java.util.Map;
-
-import org.egov.tracer.model.ServiceCallException;
-import org.springframework.core.io.ByteArrayResource;
-import org.springframework.core.io.Resource;
-import org.springframework.http.*;
-import org.springframework.stereotype.Repository;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
-import org.springframework.web.client.HttpClientErrorException;
-import org.springframework.web.client.RestTemplate;
-import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.util.UriComponentsBuilder;
-
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
-
 import lombok.extern.slf4j.Slf4j;
+import org.egov.tracer.model.ServiceCallException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.RestTemplate;
 
-@RequiredArgsConstructor
+import java.util.Map;
+
+
 @Repository
 @Slf4j
 public class ServiceRequestRepository {
 
     private final ObjectMapper mapper;
     private final RestTemplate restTemplate;
-
-    public Object fetchResult(StringBuilder uri, Object request) {
-        mapper.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
-        Object response = null;
-        try {
-            response = restTemplate.postForObject(uri.toString(), request, Map.class);
-        } catch (HttpClientErrorException e) {
-            log.error("External Service threw an Exception: ", e);
-            throw new ServiceCallException(e.getResponseBodyAsString());
-        } catch (Exception e) {
-            log.error("Exception while fetching from searcher: ", e);
-        }
-        return response;
-    }
 
 	@Autowired
 	public ServiceRequestRepository(ObjectMapper mapper, RestTemplate restTemplate) {
