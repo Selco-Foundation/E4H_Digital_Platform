3a7ba9fd259e518efba732e9f1b5aed8a53ffd09
adithyakatuku <adithya.beehyv@gmail.com>
2025-06-11 16:29:12 +0530

    Mandated commit and file attaching according to the action selected. Extra actions for send back removed.
    * Mandated commit and file attaching according to the action selected
    
    * Redundant code to used faciliate inactive sendback options removed

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/im/src/pages/employee/ComplaintDetails.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/im/src/pages/employee/ComplaintDetails.js
@@ -107,7 +107,6 @@ const ComplaintDetailsModal = ({ workflowDetails, complaintDetails, close, popup
   const [selectedReopenReason, setSelectedReopenReason] = useState(null);
   const [selectedRejectReason, setSelectedRejectReason] = useState(null);
   const [selectedSendBackReason, setSelectedSendBackReason] = useState(null);
-  const [selectedSendBackSubReason, setSelectedSendBackSubReason] = useState(null);
   const state = Digit.ULBService.getStateId();
   const reopenReasonMenu = [t(`CS_REOPEN_OPTION_ONE`), t(`CS_REOPEN_OPTION_TWO`), t(`CS_REOPEN_OPTION_THREE`), t(`CS_REOPEN_OPTION_FOUR`)];
   const { isMdmsLoading, data: rejectSendBackReasons } = Digit.Hooks.pgr.useMDMS(state, "Incident", ["RejectReasons", "SendBackReasons"]);
@@ -148,11 +147,7 @@ const ComplaintDetailsModal = ({ workflowDetails, complaintDetails, close, popup
   }
   function onSelectSendBackReason(reason) {
     setSelectedSendBackReason(reason);
-    setSelectedSendBackSubReason(null);
     setComments("");
-  }
-  function onSelectSendBackSubReason(reason) {
-    setSelectedSendBackSubReason(reason);
   }
   const clearError = useCallback(() => {
     setError("");
@@ -268,19 +263,20 @@ const ComplaintDetailsModal = ({ workflowDetails, complaintDetails, close, popup
             ? selectedSendBackReason?.additionalInputs?.[0].type === "textarea"
             : selectedRejectReason?.additionalInputs?.[0].type === "textarea");
 
-        const isCommentsMandatory = (isTextareaAction || selectedAction === "RESOLVE") && !comments.trim();
+        const isCommentsMandatory = (
+          isTextareaAction || 
+          selectedAction === "RESOLVE" || 
+          selectedAction === "OUT_OF_WARRANTY" || 
+          selectedAction === "SPARE_PART_NEEDED"
+        ) && !comments.trim();
 
         const validations = [
           { condition: selectedAction === "REJECT" && !selectedRejectReason, message: "CS_MANDATORY_DECLINE_REASON" },
           { condition: selectedAction === "SENDBACK" && !selectedSendBackReason, message: "CS_MANDATORY_SENDBACK_REASON" },
-          {
-            condition: selectedAction === "SENDBACK" && selectedSendBackReason?.additionalInputs?.[0].type === "radio" && !selectedSendBackSubReason,
-            message: "CS_MANDATORY_SENDBACK_SUBREASON",
-          },
           { condition: isCommentsMandatory, message: "CS_MANDATORY_COMMENTS" },
           { condition: selectedAction === "REOPEN" && selectedReopenReason === null, message: "CS_REOPEN_REASON_MANDATORY" },
           { condition: selectedAction === "ASSIGN" && selectedEmployee === null, message: "CS_ASSIGNEE_MANDATORY" },
-          { condition: selectedAction === "RESOLVE" && uploadedFile.length === 0, message: "CS_MANDATORY_FILE_UPLOAD" },
+          { condition: (selectedAction === "RESOLVE" || selectedAction === "OUT_OF_WARRANTY") && uploadedFile.length === 0, message: "CS_MANDATORY_FILE_UPLOAD" },
         ];
 
         const error = validations.find(({ condition }) => condition);
@@ -295,8 +291,7 @@ const ComplaintDetailsModal = ({ workflowDetails, complaintDetails, close, popup
           uploadedFile,
           selectedReopenReason,
           selectedRejectReason,
-          selectedSendBackReason,
-          selectedSendBackSubReason
+          selectedSendBackReason
         );
       }}
       error={error}
@@ -359,9 +354,7 @@ const ComplaintDetailsModal = ({ workflowDetails, complaintDetails, close, popup
           <>
             {selectedAction !== "ASSIGN" &&
             selectedAction !== "REOPEN" &&
-            !(selectedAction === "REJECT" && selectedRejectReason?.additionalInputs?.[0].type !== "textarea") &&
-            selectedAction !== "OUT_OF_WARRANTY" &&
-            selectedAction !== "SPARE_PART_NEEDED" ? (
+            !(selectedAction === "REJECT" && selectedRejectReason?.additionalInputs?.[0].type !== "textarea") ? (
               <CardLabel>{t("CS_COMMON_EMPLOYEE_COMMENTS")}*</CardLabel>
             ) : (
               <CardLabel>{t("CS_COMMON_EMPLOYEE_COMMENTS")}</CardLabel>
@@ -369,21 +362,7 @@ const ComplaintDetailsModal = ({ workflowDetails, complaintDetails, close, popup
             <TextArea name="comment" onChange={addComment} value={comments} />
           </>
         ) : null}
-        {selectedAction === "SENDBACK" && selectedSendBackReason?.additionalInputs?.[0].type === "radio" && (
-          <React.Fragment>
-            <CardLabel>{t("CS_SENDBACK_SUBCOMPLAINT")}*</CardLabel>
-            <RadioButtons
-              onSelect={onSelectSendBackSubReason}
-              selectedOption={selectedSendBackSubReason}
-              optionsKey="name"
-              options={selectedSendBackReason?.additionalInputs[0]?.options?.map((reason) => ({
-                ...reason,
-                localizedCode: t(reason.code), // Use localized text if available, otherwise fallback to default name
-              }))}
-            />
-          </React.Fragment>
-        )}
-        {selectedAction === "RESOLVE" ? (
+        {selectedAction === "RESOLVE" || selectedAction === "OUT_OF_WARRANTY" ? (
           <CardLabel>{t("CS_ACTION_SUPPORTING_DOCUMENTS")}*</CardLabel>
         ) : (
           <CardLabel>{t("CS_ACTION_SUPPORTING_DOCUMENTS")}</CardLabel>
@@ -654,8 +633,7 @@ export const ComplaintDetails = (props) => {
     uploadedFile,
     selectedReopenReason,
     selectedRejectReason,
-    selectedSendBackReason,
-    selectedSendBackSubReason
+    selectedSendBackReason
   ) {
     setPopup(false);
     const response = await Digit.Complaint.assign(
@@ -667,8 +645,7 @@ export const ComplaintDetails = (props) => {
       tenant,
       selectedReopenReason,
       selectedRejectReason,
-      selectedSendBackReason,
-      selectedSendBackSubReason
+      selectedSendBackReason
     );
     if (response?.IncidentWrappers) {
       setAssignResponse(response);
