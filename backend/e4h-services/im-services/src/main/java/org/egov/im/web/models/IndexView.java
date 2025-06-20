f9ef2a9668ceabdca9da81c15fc4c26f61eda664
Nickg-30 <154887102+Nickg-30@users.noreply.github.com>
2025-06-20 15:33:40 +0530

    adding new localized fields for dashboard (#891) (#895)
    * adding new localized fields for dashboard
    
    * resolving code rabbit comments

---
+++ backend/e4h-services/im-services/src/main/java/org/egov/im/web/models/IndexView.java
@@ -0,0 +1,33 @@
+package org.egov.im.web.models;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class IndexView {
+
+    @JsonProperty("incidentType_localized")
+    private String incidentTypeLocalized;
+
+    @JsonProperty("incidentSubType_localized")
+    private String incidentSubTypeLocalized;
+
+    @JsonProperty("applicationStatus_localized")
+    private String applicationStatusLocalized;
+
+    @JsonProperty("phcSubType_localized")
+    private String phcSubTypeLocalized;
+
+    @JsonProperty("tenantId_localized")
+    private String tenantIdLocalized;
+
+    @JsonProperty("priority")
+    private Priority priority;
+
+    @JsonProperty("state")
+    private String state;
+}
