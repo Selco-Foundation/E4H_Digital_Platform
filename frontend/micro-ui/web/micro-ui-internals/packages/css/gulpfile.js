81390fdd31f9bd7f633c225ff9d2ec7a4100b7eb
tanmaytalele <tanmaynitin.talele@beehyv.com>
2025-06-24 14:41:06 +0530

    added new fields for audit index. (#918)
    * added new fields for audit index.
    
    * Update backend/e4h-services/im-services/src/main/java/org/egov/im/service/EnrichmentService.java
    
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- frontend/micro-ui/web/micro-ui-internals/packages/css/gulpfile.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/css/gulpfile.js
@@ -59,7 +59,7 @@ function livereloadStyles() {
 exports.styles = styles;
 exports.default = series(styles);
 exports.watch = livereloadStyles;
-if (true) {
+if (process.env.NODE_ENV === "production") {
   exports.build = series(cleanStyles, styles, minify);
 } else {
   exports.build = series(styles, livereloadStyles);
