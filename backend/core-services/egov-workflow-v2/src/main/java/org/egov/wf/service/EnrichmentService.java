bee7c4636e5f9380d02d676a447602bd258c5010
tanmaytalele <tanmaynitin.talele@beehyv.com>
2025-07-04 07:24:50 +0530

    Revert "Update Null checks in EnrichmentService (#993)"
    This reverts commit 3be9e05f3fce31e92aebbad6ff9a57bb25340049.

--- backend/core-services/egov-workflow-v2/src/main/java/org/egov/wf/service/EnrichmentService.java
+++ backend/core-services/egov-workflow-v2/src/main/java/org/egov/wf/service/EnrichmentService.java
@@ -68,11 +68,8 @@ public class EnrichmentService {
             String tenantId = processStateAndAction.getProcessInstanceFromRequest().getTenantId();
             processStateAndAction.getProcessInstanceFromRequest().setId(UUID.randomUUID().toString());
             if(processStateAndAction.getAction().getNextState().equalsIgnoreCase(processStateAndAction.getAction().getCurrentState())){
-                ProcessInstance dbInstance = processStateAndAction.getProcessInstanceFromDb();
-                if (dbInstance != null && dbInstance.getAuditDetails() != null) {
-                    auditDetails.setCreatedBy(dbInstance.getAuditDetails().getCreatedBy());
-                    auditDetails.setCreatedTime(dbInstance.getAuditDetails().getCreatedTime());
-                }
+                auditDetails.setCreatedBy(processStateAndAction.getProcessInstanceFromDb().getAuditDetails().getCreatedBy());
+                auditDetails.setCreatedTime(processStateAndAction.getProcessInstanceFromDb().getAuditDetails().getCreatedTime());
             }
             processStateAndAction.getProcessInstanceFromRequest().setAuditDetails(auditDetails);
             processStateAndAction.getProcessInstanceFromRequest().setAssigner(requestInfo.getUserInfo());
