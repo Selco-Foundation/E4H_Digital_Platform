a377c0da2046e25ecac86c6d45ae154a890c7647
Babacar Ndiaye <babacar.ndiay93@gmail.com>
2025-07-09 15:48:27 +0000

    Fix issue on sla remaining value

--- backend/e4h-services/im-services-analytics/src/main/java/org/selco/e4h/service/PrioritySLAService.java
+++ backend/e4h-services/im-services-analytics/src/main/java/org/selco/e4h/service/PrioritySLAService.java
@@ -98,7 +98,7 @@ public class PrioritySLAService {
 
         String existingBusinessService =null;
         if(incidentPriorityMap.containsKey(key)) {
-             existingBusinessService = "Incident_" + incidentPriorityMap.get(key);
+             existingBusinessService = "Incident_" + capitalize(incidentPriorityMap.get(key));
         }
         else{
              existingBusinessService = "Incident_Medium";
