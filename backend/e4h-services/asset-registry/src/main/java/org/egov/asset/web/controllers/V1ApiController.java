8c0b6ed6b5680074f09ef4fc7e5c2876630126f8
teena.t <teena.t@beehyv.com>
2025-07-22 15:48:08 +0530

    modified the update asset endpoint

--- backend/e4h-services/asset-registry/src/main/java/org/egov/asset/web/controllers/V1ApiController.java
+++ backend/e4h-services/asset-registry/src/main/java/org/egov/asset/web/controllers/V1ApiController.java
@@ -143,7 +143,7 @@ public class V1ApiController {
     @RequestMapping(value = "/v1/asset/_update", method = RequestMethod.POST)
     public ResponseEntity<AssetCreateUpdateResponse> updateAsset(@Parameter(in = ParameterIn.DEFAULT, description = "Updated asset information", required = true, schema = @Schema())
         @Valid @RequestBody AssetCreateRequest body,
-        @Parameter(in = ParameterIn.PATH, description = "Unique identifier of the asset", required = true, schema = @Schema())
+        @Parameter(in = ParameterIn.QUERY, description = "Unique identifier of the asset", required = true, schema = @Schema())
         @RequestParam("assetID") String assetID) {
         validator.validateAsset(assetID, body);
         Asset updatedAsset = assetService.updateAsset(assetID, body);
