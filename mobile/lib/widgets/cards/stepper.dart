512f0d841c7e885676c22f300a5894e0601e773b
jogapps <jogappsdeveloper@gmail.com>
2025-07-22 06:22:35 +0100

    remove specification page for field staff

--- mobile/lib/widgets/cards/stepper.dart
+++ mobile/lib/widgets/cards/stepper.dart
@@ -1,26 +1,47 @@
 import 'package:digit_ui_components/theme/spacers.dart';
 import 'package:digit_ui_components/widgets/atoms/digit_stepper.dart';
 import 'package:flutter/material.dart';
-import 'package:selco/utils/extensions.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../../blocs/user_type/user_type.dart';
+import '../../utils/extensions.dart';
 
 Widget AppStepper({activeIndex = 0, required BuildContext context}) {
-  return SizedBox(
-    height: spacer8,
-    width: context.width * 0.9,
-    child: DigitStepper(
-      activeIndex: activeIndex,
-      stepperList: [
-        StepperData(
-          onStepTap: () {},
+  return BlocBuilder<UserTypeBloc, UserTypeState>(
+    builder: (context, state) {
+      final isSupervisor = state.maybeWhen(
+        supervisor: () => true,
+        orElse: () => false,
+      );
+      int actualActiveIndex = activeIndex;
+      if (!isSupervisor && activeIndex > 2) {
+        actualActiveIndex = activeIndex - 1;
+      }
+      return SizedBox(
+        height: spacer8,
+        width: context.width * 0.9,
+        child: DigitStepper(
+          activeIndex: actualActiveIndex,
+          stepperList: isSupervisor
+              ? [
+                  StepperData(onStepTap: () {}),
+                  const StepperData(),
+                  const StepperData(),
+                  const StepperData(),
+                  const StepperData(),
+                  const StepperData(),
+                ]
+              : [
+                  const StepperData(),
+                  const StepperData(),
+                  const StepperData(),
+                  const StepperData(),
+                  const StepperData(),
+                ],
+          stepperDirection: Axis.horizontal,
+          inverted: true,
         ),
-        const StepperData(),
-        const StepperData(),
-        const StepperData(),
-        const StepperData(),
-        const StepperData(),
-      ],
-      stepperDirection: Axis.horizontal,
-      inverted: true,
-    ),
+      );
+    },
   );
 }
