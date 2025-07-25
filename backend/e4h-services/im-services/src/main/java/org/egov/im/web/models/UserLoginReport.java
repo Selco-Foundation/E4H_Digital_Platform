a39638506931d0ff254189a26b98db2ab05de72f
Teena Thakur <92431551+teenathakur01@users.noreply.github.com>
2025-07-25 18:15:24 +0530

    fixed timestamp issue (#1177)

--- backend/e4h-services/im-services/src/main/java/org/egov/im/web/models/UserLoginReport.java
+++ backend/e4h-services/im-services/src/main/java/org/egov/im/web/models/UserLoginReport.java
@@ -27,7 +27,7 @@ public class UserLoginReport {
     private String currentOwnerName;
 
     @JsonProperty("lastLoginDateTime")
-    private String lastLoginDateTime;
+    private Long lastLoginDateTime;
 
     @JsonProperty("healthFacilityName")
     private String healthFacilityName;
