f33ff1b8a5d250c1d35895a093ce08163dbd92b9
Mandar-Beehyv <sanjay.mandar@beehyv.com>
2025-08-05 19:19:16 +0530

    Added new role for installation spoc.

--- backend/e4h-services/ingestion-service/app/utils/convertor.py
+++ backend/e4h-services/ingestion-service/app/utils/convertor.py
@@ -224,7 +224,7 @@ def get_installation_spoc_creation_payload(request_info: RequestInfo, name:str, 
                     "mobileNumber": mobile_number,
                     "emailId": email,
                     "roles": [
-                        {"code": "PROJECT_MANAGER", "name": "Project manager"},
+                        {"code": "INSTALLATION_REPORT_VIEWER", "name": "Installation report viewer"},
                         {"code": "HRMS_ADMIN", "name": "Hrms admin"}
                     ],
                     "tenantId": "in",
@@ -234,12 +234,12 @@ def get_installation_spoc_creation_payload(request_info: RequestInfo, name:str, 
                     {
                         "hierarchy": "ADMIN",
                         "roles": [
-                            {"value": "PROJECT_MANAGER", "label": "Project manager"},
+                            {"code": "INSTALLATION_REPORT_VIEWER", "name": "Installation report viewer"},
                             {"value": "HRMS_ADMIN", "label": "Hrms admin"}
                         ],
                         "boundaryType": "City",
                         "boundary": "in",
-                        "furnishedRolesList": "PROJECT_MANAGER, HRMS_ADMIN",
+                        "furnishedRolesList": "INSTALLATION_REPORT_VIEWER, HRMS_ADMIN",
                         "tenantId": "in",
                     }
                 ],
