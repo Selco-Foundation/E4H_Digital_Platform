f1e7be428c9375cb7662a0cb7c07105f04b0e777
Adithya Katuku <adithya.beehyv@gmail.com>
2025-08-11 19:28:39 +0530

    Fixed docs uploaded in resolve action are being showed as images on ticket timeline

--- frontend/micro-ui/web/micro-ui-internals/packages/libraries/src/utils/fileType.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/libraries/src/utils/fileType.js
@@ -2,6 +2,7 @@ const getFileTypeFromFileStoreURL = (filestoreURL) => {
   if (typeof filestoreURL === "object") {
     if (filestoreURL?.master?.includes(".m3u8")) return "hls";
   }
+  if (filestoreURL.includes(".doc")) return "doc";
   if (filestoreURL.includes(".pdf")) return "pdf";
   if (filestoreURL.includes(".jpg") || filestoreURL.includes(".jpeg") || filestoreURL.includes(".png") || filestoreURL.includes(".webp"))
     return "image";
