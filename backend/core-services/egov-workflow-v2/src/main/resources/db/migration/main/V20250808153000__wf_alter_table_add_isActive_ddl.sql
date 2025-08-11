144ea059a2bd2688655c55d4638298de1eded913
Rajat <rajat.g@beehyv.com>
2025-08-11 12:39:15 +0530

    Index on isActive=true added

--- backend/core-services/egov-workflow-v2/src/main/resources/db/migration/main/V20250808153000__wf_alter_table_add_isActive_ddl.sql
+++ backend/core-services/egov-workflow-v2/src/main/resources/db/migration/main/V20250808153000__wf_alter_table_add_isActive_ddl.sql
@@ -1,1 +1,4 @@
-ALTER TABLE public.eg_wf_processinstance_v2 ADD COLUMN isactive boolean DEFAULT true;
+ALTER TABLE public.eg_wf_processinstance_v2 ADD COLUMN isactive boolean NOT NULL DEFAULT true;
+CREATE INDEX IF NOT EXISTS idx_eg_wf_processinstance_v2_isactive_true
+    ON public.eg_wf_processinstance_v2 (isactive)
+    WHERE isactive = true;
