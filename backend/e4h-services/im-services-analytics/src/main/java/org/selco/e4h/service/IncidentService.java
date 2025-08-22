6063736b3e23f5d868c55ad22dcbde3b2080eee4
Babacar Ndiaye <babacar.ndiay93@gmail.com>
2025-08-22 07:56:35 +0000

    Update kafka listener function

--- backend/e4h-services/im-services-analytics/src/main/java/org/selco/e4h/service/IncidentService.java
+++ backend/e4h-services/im-services-analytics/src/main/java/org/selco/e4h/service/IncidentService.java
@@ -40,45 +40,52 @@ public class IncidentService {
     }
     @KafkaListener(topics = "process-audit-records")
     public void getStatusIncidentsAgregation(Map<String, Object> producerRecord) {
+        log.info("Received topic from process-audit-records");
         if(producerRecord !=null && !producerRecord.isEmpty()){
-            Object value = producerRecord.get("value");
-            IncidentRequest request = objectMapper.convertValue(value, IncidentRequest.class);
-            if (request!=null && request.getIncident()!=null){
-                String tenantId = request.getIncident().getTenantId();
-                List<IncidentStatusAgregation> statusAgregations = incidentRepository.getStatusIncidentsAgregation(tenantId);
-                List<IncidentStatusAgregation> systemFunctional = incidentRepository.getStatusSystemFunctional(tenantId);
-                if(statusAgregations !=null && !statusAgregations.isEmpty()){
-                    IncidentStatusAgregation incidentStatusAgregation = statusAgregations.get(0);
-                    // true if at least one element is NON_FUNCTIONAL
-                    boolean hasNonFunctional = systemFunctional.stream()
-                            .anyMatch(item -> NON_FUNCTIONAL.equals(item.getSystemFunctional()));
+            String topic = (String)producerRecord.get("topic");
+            if(topic !=null && !topic.isEmpty() && topic.trim().equals("save-im-request")){
+                log.info("Received topic from save-im-request");
+                Object value = producerRecord.get("value");
+                IncidentRequest request = objectMapper.convertValue(value, IncidentRequest.class);
+                if (request!=null && request.getIncident()!=null){
+                    String tenantId = request.getIncident().getTenantId();
+                    List<IncidentStatusAgregation> statusAgregations = incidentRepository.getStatusIncidentsAgregation(tenantId);
+                    List<IncidentStatusAgregation> systemFunctional = incidentRepository.getStatusSystemFunctional(tenantId);
+                    if(statusAgregations !=null && !statusAgregations.isEmpty()){
+                        IncidentStatusAgregation incidentStatusAgregation = statusAgregations.get(0);
+                        // true if at least one element is NON_FUNCTIONAL
+                        boolean hasNonFunctional = systemFunctional.stream()
+                                .anyMatch(item -> NON_FUNCTIONAL.equals(item.getSystemFunctional()));
 
-                    incidentStatusAgregation.setSystemFunctional(hasNonFunctional ? NON_FUNCTIONAL : FUNCTIONAL);
-                    incidentStatusAgregation.setLastModifiedTime(System.currentTimeMillis());
-                    Map<String, Object> tickets = esClient.getTicketsByTenantId(0, 1000, tenantId);
-                    log.info("List tickets {}", tickets.size());
-                    if(tickets!=null){
-                        Map<String, Object> source = (Map<String, Object>)tickets.get("_source");
-                        Map<String, Object> data = (Map<String, Object>)source.get("Data");
-                        String block = (String)data.get("block");
-                        String code = (String)data.get("code");
-                        String district = (String)data.get("district");
-                        boolean isLive = (boolean)data.get("isLive");
-                        String name = (String)data.get("name");
-                        String phcType = (String)data.get("phcType");
-                        String type = (String)data.get("type");
+                        incidentStatusAgregation.setSystemFunctional(hasNonFunctional ? NON_FUNCTIONAL : FUNCTIONAL);
+                        incidentStatusAgregation.setLastModifiedTime(System.currentTimeMillis());
+                        Map<String, Object> tickets = esClient.getTicketsByTenantId(0, 1000, tenantId);
+                        log.info("List tickets from tenant id {} {}", tenantId, tickets.size());
+                        if(tickets!=null){
+                            Map<String, Object> source = (Map<String, Object>)tickets.get("_source");
+                            Map<String, Object> data = (Map<String, Object>)source.get("Data");
+                            String block = (String)data.get("block");
+                            String code = (String)data.get("code");
+                            String district = (String)data.get("district");
+                            boolean isLive = (boolean)data.get("isLive");
+                            String name = (String)data.get("name");
+                            String phcType = (String)data.get("phcType");
+                            String type = (String)data.get("type");
 //                        String tenantIdES = (String)data.get("tenantId");
 
-                        incidentStatusAgregation.setBlock(block);
-                        incidentStatusAgregation.setCode(code);
-                        incidentStatusAgregation.setDistrict(district);
-                        incidentStatusAgregation.setLive(isLive);
-                        incidentStatusAgregation.setName(name);
-                        incidentStatusAgregation.setPhcType(phcType);
-                        incidentStatusAgregation.setType(type);
-                        incidentStatusAgregation.setTenantId(tenantId);
+                            incidentStatusAgregation.setBlock(block);
+                            incidentStatusAgregation.setCode(code);
+                            incidentStatusAgregation.setDistrict(district);
+                            incidentStatusAgregation.setLive(isLive);
+                            incidentStatusAgregation.setName(name);
+                            incidentStatusAgregation.setPhcType(phcType);
+                            incidentStatusAgregation.setType(type);
+                            incidentStatusAgregation.setTenantId(tenantId);
+
+                            log.info("Tickets sent to kafka {}", incidentStatusAgregation);
+                            producerService.sendIncident(config.getUpdateTopicIndexer(), incidentStatusAgregation);
+                        }
                     }
-                    producerService.sendIncident(config.getUpdateTopicIndexer(), incidentStatusAgregation);
                 }
             }
 
