9fcfc8dd2b4d2cec234588c9daea50b3184a85e1
babstar93 <140029411+babstar93@users.noreply.github.com>
2025-08-22 11:59:35 +0000

    Add count values for phc-master-list-new-2 index (#1284)
    * Add migration file to update sub types
    
    * Add indexer for project
    
    * Fix deployment issue
    
    * Fix deployment issue
    
    * Fix deployment issue
    
    * Enhance project search v2
    
    * Enhance project search v2
    
    * Enhance project search v2
    
    * Enhance project search v2
    
    * Implement inbox search for project
    
    * Fix unit test issue
    
    * Add state and district on inbox search v2
    
    * Fix issue on inbox search v2
    
    * Update project facility indexer
    
    * Update project staff indexer
    
    * Fix issue on inbox search v2
    
    * Update project staff indexer
    
    * Update project staff indexer
    
    * Update project staff indexer
    
    * Update project staff indexer
    
    * Update project staff indexer
    
    * Update project staff indexer
    
    * Update project staff indexer
    
    * Update project staff indexer
    
    * Fix case sensitive on inbox search v2
    
    * Update project staff indexer
    
    * Update code based on rabbitAI comments
    
    * Update project search to show facility in the response
    
    * Update project search to show facility in the response
    
    * Update project search to show facility in the response
    
    * Update project search to show facility in the response
    
    * Update project search to show facility in the response
    
    * Update project search
    
    * Remove migration file
    
    * returning list of process instances during search
    
    * Update code based on rabbitAI comments
    
    * Update project search to show state and district for project type facility
    
    * Update project search to show state and district for project type facility
    
    * reverted changes for list of process instances
    
    * fixed code rabbit issue
    
    * returning list of process instances during search
    
    * Update inbox search to add slaHoursRemaining and escalations as filterable fields
    
    * Add logs for index service (#1265)
    
    Co-authored-by: Babacar Ndiaye <babacar.ndiay93@gmail.com>
    
    * Update project search for boundary code
    
    * Update project search for boundary code
    
    * Add agregation count for Incident in Im-services-analytics
    
    * Add agregation count for Incident in Im-services-analytics
    
    * Add agregation count for Incident in Im-services-analytics
    
    * Add agregation count for Incident in Im-services-analytics bis
    
    * Add agregation count for Incident in Im-services-analytics controller
    
    * Comment dependancies
    
    * Update kafka listener function
    
    * Update kafka listener function
    
    * Update inbox search filter for escalation matrix
    
    ---------
    
    Co-authored-by: Babacar Ndiaye <babacar.ndiay93@gmail.com>
    Co-authored-by: teena.t <teena.t@beehyv.com>
    Co-authored-by: Teena Thakur <92431551+teenathakur01@users.noreply.github.com>

--- backend/e4h-services/im-services-analytics/src/main/java/org/selco/e4h/web/controller/IMAnalyticsController.java
+++ backend/e4h-services/im-services-analytics/src/main/java/org/selco/e4h/web/controller/IMAnalyticsController.java
@@ -3,11 +3,16 @@ package org.selco.e4h.web.controller;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.selco.e4h.kafka.consumer.KafkaProducerService;
 import org.selco.e4h.service.PrioritySLAService;
+import org.selco.e4h.web.models.IncidentRequest;
 import org.selco.e4h.web.models.SLARequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Slf4j
 @RestController
@@ -16,6 +21,7 @@ import org.springframework.web.bind.annotation.*;
 public class IMAnalyticsController {
 
     private final PrioritySLAService slaService;
+    private final KafkaProducerService producerService;
 
     @PostMapping("/computeSLA")
     public ResponseEntity<String> computeSLA(
@@ -31,6 +37,26 @@ public class IMAnalyticsController {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
                     .body("SLA computation failed: " + e.getMessage());
         }
+    }
+
+    @PostMapping("/fetchTicket")
+    public String getTicketByTenantId(@Valid @RequestBody IncidentRequest incidentRequest) {
+        Map<String, Object> producerRecord = new HashMap<>();
+        producerRecord.put("topic", "save-im-request");
+        producerRecord.put("value", incidentRequest);
+        producerService.getTicket("sk.shyagyongrumtek");
+//        producerService.sendIncident("process-audit-records", producerRecord);
+        return "User sent!";
+    }
+
+    @PostMapping("/sendObject")
+    public String sendDummyTopic(@Valid @RequestBody IncidentRequest incidentRequest) {
+        Map<String, Object> producerRecord = new HashMap<>();
+        producerRecord.put("topic", "save-im-request");
+        producerRecord.put("value", incidentRequest);
+//        producerService.getTicket("sk.shyagyongrumtek");
+        producerService.sendIncident("process-audit-records", producerRecord);
+        return "User sent!";
     }
 
 }
