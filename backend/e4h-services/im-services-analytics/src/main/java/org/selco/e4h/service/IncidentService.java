13008fef6ecbdc8e0eb20e7c5f8f45ed884a4554
Babacar Ndiaye <babacar.ndiay93@gmail.com>
2025-08-24 16:44:09 +0000

    Implement script to update phc count in existing phc

--- backend/e4h-services/im-services-analytics/src/main/java/org/selco/e4h/service/IncidentService.java
+++ backend/e4h-services/im-services-analytics/src/main/java/org/selco/e4h/service/IncidentService.java
@@ -44,56 +44,120 @@ public class IncidentService {
         if(producerRecord !=null && !producerRecord.isEmpty()){
             String topic = (String)producerRecord.get("topic");
             if(topic !=null && !topic.isEmpty() && (topic.trim().equals("save-im-request") || topic.trim().equals("update-im-request"))){
-                log.info("Received topic from save-im-request");
-                Object value = producerRecord.get("value");
-                IncidentRequest request = objectMapper.convertValue(value, IncidentRequest.class);
-                if (request!=null && request.getIncident()!=null){
-                    String tenantId = request.getIncident().getTenantId();
+                try{
+                    log.info("Received topic from save-im-request");
+                    Object value = producerRecord.get("value");
+                    IncidentRequest request = objectMapper.convertValue(value, IncidentRequest.class);
+                    if (request!=null && request.getIncident()!=null){
+                        String tenantId = request.getIncident().getTenantId();
+                        List<IncidentStatusAgregation> statusAgregations = incidentRepository.getStatusIncidentsAgregation(tenantId);
+                        List<IncidentStatusAgregation> systemFunctional = incidentRepository.getStatusSystemFunctional(tenantId);
+                        if(statusAgregations !=null && !statusAgregations.isEmpty()){
+                            IncidentStatusAgregation incidentStatusAgregation = statusAgregations.get(0);
+                            // true if at least one element is NON_FUNCTIONAL
+                            boolean hasNonFunctional = systemFunctional.stream()
+                                    .anyMatch(item -> NON_FUNCTIONAL.equals(item.getSystemFunctional()));
+
+                            incidentStatusAgregation.setSystemFunctional(hasNonFunctional ? NON_FUNCTIONAL : FUNCTIONAL);
+                            incidentStatusAgregation.setLastModifiedTime(System.currentTimeMillis());
+                            Map<String, Object> tickets = esClient.getHFByTenantId(0, 1000, tenantId);
+                            log.info("List tickets from tenant id {} {}", tenantId, tickets.size());
+                            if(tickets!=null){
+                                Map<String, Object> source = (Map<String, Object>)tickets.get("_source");
+                                Map<String, Object> data = (Map<String, Object>)source.get("Data");
+                                String block = (String)data.get("block");
+                                String code = String.valueOf(data.get("code"));
+                                String state = (String)data.get("state");
+                                String district = (String)data.get("district");
+                                boolean isLive = (boolean)data.get("isLive");
+                                String name = (String)data.get("name");
+                                String phcType = (String)data.get("phcType");
+                                String type = (String)data.get("type");
+                                String tenantIdLocalized = (String)data.get("tenantId_localized");
+                                List<Double> geoPoint = (List<Double>) data.get("geo-point");
+
+                                incidentStatusAgregation.setBlock(block);
+                                incidentStatusAgregation.setCode(code);
+                                incidentStatusAgregation.setDistrict(district);
+                                incidentStatusAgregation.setLive(isLive);
+                                incidentStatusAgregation.setName(name);
+                                incidentStatusAgregation.setPhcType(phcType);
+                                incidentStatusAgregation.setType(type);
+                                incidentStatusAgregation.setTenantId(tenantId);
+                                incidentStatusAgregation.setTenantIdLocalized(tenantIdLocalized);
+                                incidentStatusAgregation.setGeoPoint(geoPoint);
+                                incidentStatusAgregation.setState(state);
+
+                                log.info("Tickets sent to kafka {}", incidentStatusAgregation);
+                                producerService.sendIncident(config.getUpdateTopicIndexer(), incidentStatusAgregation);
+                            }
+                        }
+                    }
+                }
+                catch (Exception e){
+                    e.printStackTrace();
+                }
+            }
+
+        }
+    }
+
+    public void scriptUpdatePHCAgregation() {
+        log.info("Script function called");
+        try{
+            List<Map<String, Object>> listPHCs = esClient.getAllPHC(0, 10);
+            log.info("List tickets size {}", listPHCs.size());
+            if(listPHCs!=null && !listPHCs.isEmpty()){
+                for (Map<String, Object> phc : listPHCs){
+                    Map<String, Object> data = (Map<String, Object>)phc.get("Data");
+                    String block = (String)data.get("block");
+                    String code = String.valueOf(data.get("code"));
+                    String state = (String)data.get("state");
+                    String district = (String)data.get("district");
+                    boolean isLive = (boolean)data.get("isLive");
+                    String name = (String)data.get("name");
+                    String phcType = (String)data.get("phcType");
+                    String type = (String)data.get("type");
+                    String tenantId = (String)data.get("tenantId");
+                    String tenantIdLocalized = (String)data.get("tenantId_localized");
+                    List<Double> geoPoint = (List<Double>) data.get("geo-point");
+
+                    IncidentStatusAgregation incidentStatusAgregation = new IncidentStatusAgregation();
+                    incidentStatusAgregation.setBlock(block);
+                    incidentStatusAgregation.setCode(code);
+                    incidentStatusAgregation.setDistrict(district);
+                    incidentStatusAgregation.setLive(isLive);
+                    incidentStatusAgregation.setName(name);
+                    incidentStatusAgregation.setPhcType(phcType);
+                    incidentStatusAgregation.setType(type);
+                    incidentStatusAgregation.setTenantId(tenantId);
+                    incidentStatusAgregation.setTenantIdLocalized(tenantIdLocalized);
+                    incidentStatusAgregation.setGeoPoint(geoPoint);
+                    incidentStatusAgregation.setState(state);
+
                     List<IncidentStatusAgregation> statusAgregations = incidentRepository.getStatusIncidentsAgregation(tenantId);
                     List<IncidentStatusAgregation> systemFunctional = incidentRepository.getStatusSystemFunctional(tenantId);
                     if(statusAgregations !=null && !statusAgregations.isEmpty()){
-                        IncidentStatusAgregation incidentStatusAgregation = statusAgregations.get(0);
+                        IncidentStatusAgregation incidentStatusAgregationDB = statusAgregations.get(0);
                         // true if at least one element is NON_FUNCTIONAL
                         boolean hasNonFunctional = systemFunctional.stream()
                                 .anyMatch(item -> NON_FUNCTIONAL.equals(item.getSystemFunctional()));
 
+                        incidentStatusAgregation.setTotalOccurences(incidentStatusAgregationDB.getTotalOccurences());
+                        incidentStatusAgregation.setTotalOpenOccurrences(incidentStatusAgregationDB.getTotalOpenOccurrences());
+                        incidentStatusAgregation.setTotalCloseOccurrences(incidentStatusAgregationDB.getTotalCloseOccurrences());
                         incidentStatusAgregation.setSystemFunctional(hasNonFunctional ? NON_FUNCTIONAL : FUNCTIONAL);
                         incidentStatusAgregation.setLastModifiedTime(System.currentTimeMillis());
-                        Map<String, Object> tickets = esClient.getTicketsByTenantId(0, 1000, tenantId);
-                        log.info("List tickets from tenant id {} {}", tenantId, tickets.size());
-                        if(tickets!=null){
-                            Map<String, Object> source = (Map<String, Object>)tickets.get("_source");
-                            Map<String, Object> data = (Map<String, Object>)source.get("Data");
-                            String block = (String)data.get("block");
-                            String code = (String)data.get("code");
-                            String state = (String)data.get("state");
-                            String district = (String)data.get("district");
-                            boolean isLive = (boolean)data.get("isLive");
-                            String name = (String)data.get("name");
-                            String phcType = (String)data.get("phcType");
-                            String type = (String)data.get("type");
-                            String tenantIdLocalized = (String)data.get("tenantId_localized");
-                            List<Double> geoPoint = (List<Double>) data.get("geo-point");
 
-                            incidentStatusAgregation.setBlock(block);
-                            incidentStatusAgregation.setCode(code);
-                            incidentStatusAgregation.setDistrict(district);
-                            incidentStatusAgregation.setLive(isLive);
-                            incidentStatusAgregation.setName(name);
-                            incidentStatusAgregation.setPhcType(phcType);
-                            incidentStatusAgregation.setType(type);
-                            incidentStatusAgregation.setTenantId(tenantId);
-                            incidentStatusAgregation.setTenantIdLocalized(tenantIdLocalized);
-                            incidentStatusAgregation.setGeoPoint(geoPoint);
-                            incidentStatusAgregation.setState(state);
-
-                            log.info("Tickets sent to kafka {}", incidentStatusAgregation);
-                            producerService.sendIncident(config.getUpdateTopicIndexer(), incidentStatusAgregation);
-                        }
                     }
+
+                    log.info("Tickets sent to kafka {}", incidentStatusAgregation);
+                    producerService.sendIncident(config.getUpdateTopicIndexer(), incidentStatusAgregation);
                 }
             }
-
+        }
+        catch (Exception e){
+            e.printStackTrace();
         }
     }
 }
--- backend/e4h-services/im-services-analytics/src/main/java/org/selco/e4h/web/controller/IMAnalyticsController.java
+++ backend/e4h-services/im-services-analytics/src/main/java/org/selco/e4h/web/controller/IMAnalyticsController.java
@@ -4,6 +4,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.selco.e4h.kafka.consumer.KafkaProducerService;
+import org.selco.e4h.service.IncidentService;
 import org.selco.e4h.service.PrioritySLAService;
 import org.selco.e4h.web.models.IncidentRequest;
 import org.selco.e4h.web.models.SLARequest;
@@ -22,6 +23,7 @@ public class IMAnalyticsController {
 
     private final PrioritySLAService slaService;
     private final KafkaProducerService producerService;
+    private final IncidentService incidentService;
 
     @PostMapping("/computeSLA")
     public ResponseEntity<String> computeSLA(
@@ -39,14 +41,10 @@ public class IMAnalyticsController {
         }
     }
 
-    @PostMapping("/fetchTicket")
-    public String getTicketByTenantId(@Valid @RequestBody IncidentRequest incidentRequest) {
-        Map<String, Object> producerRecord = new HashMap<>();
-        producerRecord.put("topic", "save-im-request");
-        producerRecord.put("value", incidentRequest);
-        producerService.getTicket("sk.shyagyongrumtek");
-//        producerService.sendIncident("process-audit-records", producerRecord);
-        return "User sent!";
+    @GetMapping("/update_phc")
+    public String getTicketByTenantId() {
+        incidentService.scriptUpdatePHCAgregation();
+        return "Script started!";
     }
 
     @PostMapping("/sendObject")
