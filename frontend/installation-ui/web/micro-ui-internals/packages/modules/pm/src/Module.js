8ddcb8525c654cd474d8813b25274a5c728365ec
adithyakatuku <adithya.beehyv@gmail.com>
2025-08-29 20:15:06 +0530

    Added project manager ui module

---
+++ frontend/installation-ui/web/micro-ui-internals/packages/modules/pm/src/Module.js
@@ -0,0 +1,56 @@
+import React, { useEffect } from "react";
+import getRootReducer from "./redux/reducers";
+import EmployeeApp from "./App";
+import { useTranslation } from "react-i18next";
+import { LOCALE } from "./constants/Localization";
+import { ComplaintIcon, Loader, CitizenHomeCard } from "@egovernments/digit-ui-react-components";
+import PMCard from "./components/PMCard";
+
+export const PMReducers = getRootReducer;
+
+const PMModule = ({ stateCode, userType, tenants }) => {
+  const moduleCode = "PM";
+  const language = Digit.StoreData.getCurrentLanguage();
+  const { isLoading, data: store } = Digit.Services.useStore({ stateCode, moduleCode, language });
+
+  if (isLoading) {
+    return <Loader />;
+  }
+
+  Digit.SessionStorage.set("PM_TENANTS", tenants);
+  return <EmployeeApp />;
+};
+
+const PMLinks = ({ matchPath }) => {
+  const { t } = useTranslation();
+  const [params, setParams, clearParams] = Digit.Hooks.useSessionStorage("PGR_CITIZEN_CREATE_COMPLAINT", {});
+
+  useEffect(() => {
+    clearParams();
+  }, []);
+
+  const links = [
+    {
+      link: `${matchPath}/create-complaint/complaint-type`,
+      i18nKey: t("CS_COMMON_FILE_A_COMPLAINT"),
+    },
+    {
+      link: `${matchPath}/complaints`,
+      i18nKey: t(LOCALE.MY_COMPLAINTS),
+    },
+  ];
+
+  return <CitizenHomeCard header={t("CS_COMMON_HOME_COMPLAINTS")} links={links} Icon={ComplaintIcon} />;
+};
+
+const componentsToRegister = {
+  PMModule,
+  PMLinks,
+  PMCard,
+};
+
+export const initPMComponents = () => {
+  Object.entries(componentsToRegister).forEach(([key, value]) => {
+    Digit.ComponentRegistryService.setComponent(key, value);
+  });
+};
---
+++ frontend/installation-ui/web/micro-ui-internals/packages/modules/pm/src/redux/reducers/commonreducer.js
@@ -0,0 +1,7 @@
+
+export const commonReducer = (defaultData) => (state = defaultData, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
