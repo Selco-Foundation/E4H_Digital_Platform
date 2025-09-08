8bcd7217751ac41422e70e871db21198d650349b
adithyakatuku <adithya.beehyv@gmail.com>
2025-09-08 20:59:28 +0530

    Home cards styling changes

--- frontend/installation-ui/web/micro-ui-internals/packages/modules/pm/src/components/PMCard.js
+++ frontend/installation-ui/web/micro-ui-internals/packages/modules/pm/src/components/PMCard.js
@@ -30,95 +30,81 @@ const PMCard = () => {
         marginLeft: "0px",
         margin: userType === "citizen" ? "8px" : "0px",
         gap: userType === "citizen" ? "" : "0 24px",
-        boxShadow: userType === "citizen" ? "1px 1px 4px 0px rgba(0,0,0,0.2)" : "",
-        background: userType === "citizen" ? "white" : "",
-        borderRadius: userType === "citizen" ? "4px" : "",
-        width: "350px",
+        boxShadow: "1px 1px 4px 0px rgba(0,0,0,0.2)",
+        backgroundColor: "white",
+        borderRadius: "4px",
+        width: "320px",
         maxWidth: "95%",
-        minHeight: "200px",
+        minHeight: "297px",
+        position: "relative",
+        padding: "24px",
       }}
     >
-      <section
+      <div
         style={{
-          position: "relative",
-          height: "100%",
-          borderRadius: "4px",
-          boxShadow: userType === "citizen" ? "" : "1px 1px 4px 0px rgba(0,0,0,0.2)",
-          background: "white",
-          padding: userType === "citizen" ? "8px" : "16px",
+          marginBottom: "20px",
+          padding: "8px 0px 25px 0px",
+          display: "flex",
+          gap: "10px",
+          alignItems: "center",
+          lineHeight: "35px",
+          borderBottom: "1px solid #D6D5D4",
         }}
       >
+        <AdminPanelSettings height={"28px"} width={"28px"} />
         <div
           style={{
-            marginBottom: "10px",
-            padding: "8px",
-            paddingLeft: 0,
+            fontFamily: "Roboto",
+            fontWeight: "700",
+            fontSize: "24px",
+            lineHeight: "100%",
+            letterSpacing: "0px",
+            color: "#0B4B66",
+            width: "70%",
+          }}
+        >
+          {t("CS_COMMON_PROJECTS")}
+        </div>
+      </div>
+      <div>
+        <div
+          style={{
             display: "flex",
-            gap: "10px",
+            gap: "8px",
             alignItems: "center",
-            borderBottom: "1px solid #D6D5D4",
+            color: "#C84C0E",
+            cursor: "pointer",
+            marginBottom: "8px",
+            fontSize: "16px",
+            fontWeight: "bold",
+            fontFamily: "Roboto",
           }}
+          onClick={handleCreateProjectRedirection}
         >
-          <AdminPanelSettings height={"28px"} width={"28px"} />
-          <div
-            style={{
-              fontFamily: "Roboto",
-              fontWeight: "700",
-              fontSize: "24px",
-              lineHeight: "100%",
-              letterSpacing: "0px",
-              color: "#0B4B66",
-              width: "70%",
-            }}
-          >
-            {t("CS_COMMON_PROJECTS")}
-          </div>
+          <AddExpense />
+          <span>
+            {t("QC_ACTION_CREATE_PROJECT")}
+          </span>
         </div>
         <div
           style={{
-            padding: "8px",
-            paddingLeft: 0,
+            display: "flex",
+            gap: "8px",
+            alignItems: "center",
+            color: "#C84C0E",
+            cursor: "pointer",
+            fontSize: "16px",
+            fontWeight: "bold",
+            fontFamily: "Roboto",
           }}
+          onClick={handleViewProjectRedirection}
         >
-          <div
-            style={{
-              display: "flex",
-              gap: "8px",
-              alignItems: "center",
-              color: "#C84C0E",
-              cursor: "pointer",
-              marginBottom: "8px",
-              fontSize: "16px",
-              fontWeight: "bold",
-              fontFamily: "Roboto",
-            }}
-            onClick={handleCreateProjectRedirection}
-          >
-            <AddExpense />
-            <span>
-              {t("QC_ACTION_CREATE_PROJECT")}
-            </span>
-          </div>
-          <div
-            style={{
-              display: "flex",
-              gap: "8px",
-              alignItems: "center",
-              color: "#C84C0E",
-              cursor: "pointer",
-              fontSize: "16px",
-              fontWeight: "bold",
-              fontFamily: "Roboto",
-            }}
-            onClick={handleViewProjectRedirection}
-          >
-            <ListAlt />
-            <span>
-              {t("QC_ACTION_VIEW_PROJECTS")}
-            </span>
-          </div>
+          <ListAlt />
+          <span>
+            {t("QC_ACTION_VIEW_PROJECTS")}
+          </span>
         </div>
-      </section>
+      </div>
     </div>
   );
 }
--- frontend/installation-ui/web/micro-ui-internals/packages/modules/qc/src/components/QCCard.js
+++ frontend/installation-ui/web/micro-ui-internals/packages/modules/qc/src/components/QCCard.js
@@ -26,77 +26,80 @@ const QCCard = () => {
         marginLeft: "0px",
         margin: userType === "citizen" ? "8px" : "0px",
         gap: userType === "citizen" ? "" : "0 24px",
-        boxShadow: userType === "citizen" ? "1px 1px 4px 0px rgba(0,0,0,0.2)" : "",
-        background: userType === "citizen" ? "white" : "",
-        borderRadius: userType === "citizen" ? "4px" : "",
-        width: "350px",
+        boxShadow: "1px 1px 4px 0px rgba(0,0,0,0.2)",
+        backgroundColor: "white",
+        borderRadius: "4px",
+        width: "320px",
         maxWidth: "95%",
-        minHeight: "200px",
+        minHeight: "297px",
+        position: "relative",
+        padding: "24px",
       }}
     >
-      <section
+      <div
         style={{
-          position: "relative",
-          height: "100%",
-          borderRadius: "4px",
-          boxShadow: userType === "citizen" ? "" : "1px 1px 4px 0px rgba(0,0,0,0.2)",
-          background: "white",
-          padding: userType === "citizen" ? "8px" : "16px",
+          marginBottom: "20px",
+          padding: "8px 0px 27px 0px",
+          display: "flex",
+          gap: "16px",
+          alignItems: "center",
+          lineHeight: "35px",
+          borderBottom: "1px solid #D6D5D4",
         }}
       >
-        <div style={{ marginBottom: "10px", padding: "8px", paddingLeft: 0, display: "flex", gap: "16px", alignItems: "center" }}>
-          <CustomMenuIcon color="#B91900" />
-          <div
-            style={{
-              fontFamily: "Roboto",
-              fontWeight: "700",
-              fontSize: "24px",
-              lineHeight: "100%",
-              letterSpacing: "0px",
-              color: "#0B4B66",
-              width: "70%",
-            }}
-          >
-            {t("CS_COMMON_INBOX")}
-          </div>
-        </div>
+        <CustomMenuIcon color="#B91900" />
         <div
           style={{
-            marginBottom: "10px",
             fontFamily: "Roboto",
-            fontWeight: 400,
-            fontSize: "16px",
-            lineHeight: "24px",
+            fontWeight: "700",
+            fontSize: "24px",
+            lineHeight: "100%",
             letterSpacing: "0px",
-            color: "#0B0C0C",
+            color: "#0B4B66",
+            width: "70%",
           }}
         >
-          {t("CS_COMMON_HOME_INBOX_DESC")}
+          {t("CS_COMMON_INBOX")}
         </div>
-        <div
-          style={{
-            width: 116,
-            height: 32,
-            display: "flex",
-            gap: "8px",
-            justifyContent: "center",
-            alignItems: "center",
-            paddingTop: "8px",
-            paddingRight: "20px",
-            paddingBottom: "8px",
-            paddingLeft: "20px",
-            background: "#C84C0E",
-            color: "white",
-            cursor: "pointer",
-          }}
-          onClick={handleClick}
-        >
-          <span>
-            {t("CORE_COMMON_VIEW")}
-          </span>
-          <CustomArrowRight color={"white"} height={"14px"} width={"14px"} />
-        </div>
-      </section>
+      </div>
+      <div
+        style={{
+          marginBottom: "10px",
+          fontFamily: "Roboto",
+          fontWeight: 400,
+          fontSize: "16px",
+          lineHeight: "24px",
+          letterSpacing: "0px",
+          color: "#0B0C0C",
+        }}
+      >
+        {t("CS_COMMON_HOME_INBOX_DESC")}
+      </div>
+      <div
+        style={{
+          width: 116,
+          height: 32,
+          display: "flex",
+          gap: "8px",
+          justifyContent: "center",
+          alignItems: "center",
+          paddingTop: "8px",
+          paddingRight: "20px",
+          paddingBottom: "8px",
+          paddingLeft: "20px",
+          background: "#C84C0E",
+          color: "white",
+          cursor: "pointer",
+          position: "absolute",
+          bottom: "24px",
+        }}
+        onClick={handleClick}
+      >
+        <span>
+          {t("CORE_COMMON_VIEW")}
+        </span>
+        <CustomArrowRight color={"white"} height={"14px"} width={"14px"} />
+      </div>
     </div>
   );
 }
