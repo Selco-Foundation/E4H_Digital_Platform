b79ed48cdb698e6c0dfbcdeb24ca6f69d69fa5c8
Pasam6 <Prasad.pasam@beehyv.com>
2025-09-08 23:23:25 +0530

    co-pilot

--- backend/e4h-services/im-services/src/main/resources/db/migrate.sh
+++ backend/e4h-services/im-services/src/main/resources/db/migrate.sh
@@ -4,7 +4,7 @@ CLASSPATH="/flyway/jars"
 for jar in /flyway/jars/*.jar; do
   CLASSPATH="$CLASSPATH:$jar"
 done
-CLASSPATH="$CLASSPATH:/flyway/flyway-cli/flyway-10.7.1/lib/*"
+CLASSPATH="$CLASSPATH:/flyway/flyway-cli/flyway-10.7.1/lib/flyway/*"
 
 exec java -cp "$CLASSPATH" org.flywaydb.commandline.Main \
   -url=$DB_URL \
