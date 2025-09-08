2c2b61d70a7485b9ac0356ecb781468ff6116f34
Pasam6 <Prasad.pasam@beehyv.com>
2025-09-08 16:50:20 +0530

    co-pilot

--- backend/e4h-services/im-services/src/main/resources/db/migrate.sh
+++ backend/e4h-services/im-services/src/main/resources/db/migrate.sh
@@ -1,3 +1,18 @@
 #!/bin/sh
 
-flyway -url=$DB_URL -table=$SCHEMA_TABLE -user=$FLYWAY_USER -password=$FLYWAY_PASSWORD -locations=$FLYWAY_LOCATIONS -baselineOnMigrate=true -outOfOrder=true  migrate
+# Build classpath: all jars and classes in /flyway/jars
+CLASSPATH="/flyway/jars"
+for jar in /flyway/jars/*.jar; do
+  CLASSPATH="$CLASSPATH:$jar"
+done
+CLASSPATH="$CLASSPATH:/flyway/lib/*"
+
+exec java -cp $CLASSPATH org.flywaydb.commandline.Main \
+  -url=$DB_URL \
+  -table=$SCHEMA_TABLE \
+  -user=$FLYWAY_USER \
+  -password=$FLYWAY_PASSWORD \
+  -locations=$FLYWAY_LOCATIONS \
+  -baselineOnMigrate=true \
+  -outOfOrder=true \
+  migrate
