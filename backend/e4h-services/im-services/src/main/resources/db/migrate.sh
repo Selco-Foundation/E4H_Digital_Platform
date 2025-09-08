8009282ec79f97723391470381b6adf7ac9fefe4
Pasam6 <Prasad.pasam@beehyv.com>
2025-09-08 23:57:08 +0530

    flyway version fix

--- backend/e4h-services/im-services/src/main/resources/db/migrate.sh
+++ backend/e4h-services/im-services/src/main/resources/db/migrate.sh
@@ -4,7 +4,7 @@ CLASSPATH="/flyway/jars"
 for jar in /flyway/jars/*.jar; do
   CLASSPATH="$CLASSPATH:$jar"
 done
-CLASSPATH="$CLASSPATH:/flyway/flyway-cli/flyway-9.22.3/lib/community/*"
+CLASSPATH="$CLASSPATH:/flyway/flyway-cli/flyway-9.22.3/lib/*:/flyway/flyway-cli/flyway-9.22.3/lib/community/*"
 
 exec java -cp "$CLASSPATH" org.flywaydb.commandline.Main \
   -url=$DB_URL \
