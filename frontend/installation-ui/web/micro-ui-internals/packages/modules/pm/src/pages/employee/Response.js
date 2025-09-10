ee452f5c84d64a34d2d5c76ef7a65d4541cf65a3
jogapps <jogappsdeveloper@gmail.com>
2025-09-10 09:26:25 +0100

    removed unnecessary variable and customized component

--- frontend/installation-ui/web/micro-ui-internals/packages/modules/pm/src/pages/employee/Response.js
+++ frontend/installation-ui/web/micro-ui-internals/packages/modules/pm/src/pages/employee/Response.js
@@ -1,47 +1,31 @@
 import React from "react";
-import { Card, Banner, CardText, SubmitBar } from "@egovernments/digit-ui-react-components";
-import { Link, useRouteMatch } from "react-router-dom";
+import { Card, Banner, SubmitBar } from "@egovernments/digit-ui-react-components";
+import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { useTranslation } from "react-i18next";
 
-const GetActionMessage = ({ action }) => {
-    const { t } = useTranslation();
-    if (action === "REOPEN") {
-        return t(`CS_COMMON_COMPLAINT_REOPENED`);
-    } else if (action === "RATE") {
-        return t(`CS_COMMON_COMPLAINT_RATED`);
-    } else {
-        return t(`PM_LABEL_PROJECT_CREATED`);
-    }
-};
-
-const BannerPicker = ({ response }) => {
-    const { t } = useTranslation();
-    console.log("response", response)
+const BannerPicker = ({ response, message, complaintNumber }) => {
 
     if (response) {
         return (
             <Banner
-                message={GetActionMessage("SUBMITTED")}
-                complaintNumber={"1234567890"}
+                message={message}
+                complaintNumber={complaintNumber}
                 successful={true}
                 whichSvg="tick"
             />
         );
     } else {
-        return <Banner message={t("CS_COMMON_COMPLAINT_NOT_SUBMITTED")} successful={false} />;
+        return <Banner message={message} successful={false} />;
     }
 };
 
 const Response = (props) => {
     const { t } = useTranslation();
-    const { match } = useRouteMatch();
     const appState = useSelector((state) => state)["pm"];
-    sessionStorage.removeItem("complaintType");
-    sessionStorage.removeItem("subType");
     return (
         <Card>
-            <BannerPicker response={appState} />
+            <BannerPicker response={appState} message={appState.message} complaintNumber={appState.complaintNumber}/>
             <div style={{
                 display: "flex",
                 alignItems: "center",
@@ -49,17 +33,27 @@ const Response = (props) => {
                 gap: "16px"
             }}>
                 <Link to={`/${window.contextPath}/employee`} style={{ textDecoration: "none" }}>
-                    <SubmitBar label={t("CS_COMMON_HOME")}
-                               style={{
-                                   backgroundColor: "transparent",
-                                   color: "#C84C0E" ,
-                                   border: "1px solid #C84C0E",
-                                   boxShadow: "none",
-                                   fontWeight: "600"
-                               }} />
+                    <div style={{
+                        backgroundColor: "transparent",
+                        color: "#C84C0E",
+                        border: "1px solid #C84C0E",
+                        boxShadow: "none",
+                        fontWeight: "600",
+                        padding: "10px 24px",
+                        minWidth: "200px",
+                        display: "flex",
+                        justifyContent: "center",
+                        alignItems: "center",
+                        cursor: "pointer",
+                        fontSize: "16px",
+                        textAlign: "center"
+                    }}>
+                        {t("CS_COMMON_HOME")}
+                    </div>
                 </Link>
                 <Link to={`/${window.contextPath}/employee`}>
-                    <SubmitBar label={t("PM_LABEL_CREATE_FIELD_PLAN")} />
+                    <SubmitBar label={t("PM_LABEL_CREATE_FIELD_PLAN")}
+                               style={{ boxShadow: "none" }}/>
                 </Link>
             </div>
         </Card>
