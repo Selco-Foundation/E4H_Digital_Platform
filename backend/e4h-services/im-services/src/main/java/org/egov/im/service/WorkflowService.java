1378a03276e907ea9351144deccde66440104615
tanmaytalele <tanmaynitin.talele@beehyv.com>
2025-09-11 13:33:03 +0530

    removed the if condition for closed tickets (#1396)
    * removed the if condition for closed tickets
    
    * space

--- backend/e4h-services/im-services/src/main/java/org/egov/im/service/WorkflowService.java
+++ backend/e4h-services/im-services/src/main/java/org/egov/im/service/WorkflowService.java
@@ -146,12 +146,6 @@ public class WorkflowService {
         //get all process instances
         List<ProcessInstance> processInstances = getAllProcessInstances(tenantId,IncidentId, requestInfo);
         Collections.reverse(processInstances);
-
-        if((applicationStatus.equals(CLOSED_AFTER_REJECTION) || applicationStatus.equals(CLOSED_AFTER_RESOLUTION)) && !processInstances.isEmpty()){
-            long prevStateTime = processInstances.get(0).getAuditDetails().getCreatedTime();
-            ZonedDateTime zonedPrevStateTime = ZonedDateTime.ofInstant(Instant.ofEpochMilli(prevStateTime), ZoneId.of("Asia/Kolkata"));
-            now = zonedPrevStateTime;
-        }
         // Step 3: Use BusinessHoursUtil
         BusinessHoursUtil util = new BusinessHoursUtil(businessHourList);
         long businessHoursElapsed = util.calculateBusinessDurationForAllStates(processInstances);
