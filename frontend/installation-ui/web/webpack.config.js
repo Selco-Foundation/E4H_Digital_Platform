33855b116df4206e2a8e4b93757611b264d2d617
vinothrallapalli-egov <vinoth.r@egovernments.org>
2025-09-11 17:16:22 +0530

    Changed context path to  installation-qc

--- frontend/installation-ui/web/webpack.config.js
+++ frontend/installation-ui/web/webpack.config.js
@@ -31,7 +31,7 @@ module.exports = {
   output: {
     filename: "[name].bundle.js",
     path: path.resolve(__dirname, "build"),
-    publicPath: "/workbench-ui/",
+    publicPath: "/installation-qc/",
   },
   optimization: {
     splitChunks: {
