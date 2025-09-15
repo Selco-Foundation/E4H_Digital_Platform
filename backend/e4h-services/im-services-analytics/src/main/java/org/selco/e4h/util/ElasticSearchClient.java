ba51a82994a39a3e7e23180d93706cfa8e18e7ca
Aryan Tiwari <162558189+AryanT97@users.noreply.github.com>
2025-09-15 15:09:29 +0530

    Indexer update (#1408)
    * Updating the computed-sla-im-services
    
    * Updating error fix
    
    * Updating error fix 2

--- backend/e4h-services/im-services-analytics/src/main/java/org/selco/e4h/util/ElasticSearchClient.java
+++ backend/e4h-services/im-services-analytics/src/main/java/org/selco/e4h/util/ElasticSearchClient.java
@@ -33,10 +33,13 @@ public class ElasticSearchClient {
     private String phcIndex;
 
     private static final String SEARCH_PATH = "_search";
-    private static final String INDEX_NAME = computedSlaImServicesIndex;
     private static final String OLD_INDEX_NAME = "im-services";
-
+    private String INDEX_NAME;
     private static final String INDEX_NAME_PHC = "phc-master-list-new-2";
+
+    private void init() {
+        this.INDEX_NAME = computedSlaImServicesIndex;
+    }
 
     private static final String DOC_PATH = "_doc";
 
