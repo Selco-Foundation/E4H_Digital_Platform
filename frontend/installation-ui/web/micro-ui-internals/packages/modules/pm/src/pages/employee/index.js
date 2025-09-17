f8369d455268b36f7794789e28626243bcfd2c16
adithyakatuku <adithya.beehyv@gmail.com>
2025-09-17 11:15:10 +0530

    Breadcrumbs styling changes

--- frontend/installation-ui/web/micro-ui-internals/packages/modules/pm/src/pages/employee/index.js
+++ frontend/installation-ui/web/micro-ui-internals/packages/modules/pm/src/pages/employee/index.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from "react";
 import { useTranslation } from "react-i18next";
 import { Route, Switch, useRouteMatch } from "react-router-dom";
-import { BreadCrumb } from "@egovernments/digit-ui-react-components";
+import { BreadCrumb } from "@egovernments/digit-ui-components";
 import { useSelector } from "react-redux";
 import CreateProject from "./CreateProject";
 import Response from "./Response";
@@ -17,32 +17,32 @@ const PMApp = () => {
   const breadCrumbsConfig = {
     home: {
       content: t("CS_COMMON_HOME"),
-      path: `/${window.contextPath}/employee`,
+      internalLink: `/${window.contextPath}/employee`,
       show: true,
     },
     projectCreation: {
       content: t("PM_ACTION_CREATE_PROJECT"),
-      path: match.url + `/project/create`,
+      internalLink: match.url + `/project/create`,
       show: true,
     },
     response: {
       content: t("CORE_COMMON_RESPONSE"),
-      path: match.url + `/response`,
+      internalLink: match.url + `/response`,
       show: true,
     },
     fieldPlanCreation: {
       content: t("PM_ACTION_CREATE_FIELD_PLAN"),
-      path: match.url + `/project/1234567890/field-plan/create`,
+      internalLink: match.url + `/project/1234567890/field-plan/create`,
       show: true,
     },
     projectDetails: {
       content: t("PM_PROJECT_PROJECT_DETAILS"),
-      path: match.url + `/project/1234567890/details`,
+      internalLink: match.url + `/project/1234567890/details`,
       show: true,
     },
     project: {
       content: t("PM_LABEL_MY_PROJECTS"),
-      path: match.url + `/projects`,
+      internalLink: match.url + `/projects`,
       show: true,
     }
   };
@@ -55,23 +55,38 @@ const PMApp = () => {
     <div className="ground-container">
       <Switch>
         <Route path={`${path}/project/create`} exact={true}>
-          <BreadCrumb crumbs={[breadCrumbsConfig.home, breadCrumbsConfig.projectCreation]} />
+          <BreadCrumb
+            spanStyle={{ color: "#0B0C0C" }}
+            crumbs={[breadCrumbsConfig.home, breadCrumbsConfig.projectCreation]}
+          />
           <CreateProject />
         </Route>
         <Route path={`${path}/project/:projectId/details`} exact={true}>
-          <BreadCrumb crumbs={[breadCrumbsConfig.home, breadCrumbsConfig.projectDetails]} />
+          <BreadCrumb
+            spanStyle={{ color: "#0B0C0C" }}
+            crumbs={[breadCrumbsConfig.home, breadCrumbsConfig.projectDetails]}
+          />
           <ProjectDetails />
         </Route>
         <Route path={`${path}/project/:projectId/field-plan/create`} exact={true}>
-          <BreadCrumb crumbs={[breadCrumbsConfig.home, breadCrumbsConfig.fieldPlanCreation]} />
+          <BreadCrumb
+            spanStyle={{ color: "#0B0C0C" }}
+            crumbs={[breadCrumbsConfig.home, breadCrumbsConfig.fieldPlanCreation]}
+          />
           <CreateFieldPlan />
         </Route>
         <Route path={`${path}/response`} exact={true}>
-          <BreadCrumb crumbs={[breadCrumbsConfig.home, breadCrumbsConfig.response]} />
+          <BreadCrumb
+            spanStyle={{ color: "#0B0C0C" }}
+            crumbs={[breadCrumbsConfig.home, breadCrumbsConfig.response]}
+          />
           <Response />
         </Route>
         <Route path={`${path}/projects`} exact={true}>
-          <BreadCrumb crumbs={[breadCrumbsConfig.home, breadCrumbsConfig.project]} />
+          <BreadCrumb
+            spanStyle={{ color: "#0B0C0C" }}
+            crumbs={[breadCrumbsConfig.home, breadCrumbsConfig.project]}
+          />
           <ProjectTable />
         </Route>
       </Switch>
