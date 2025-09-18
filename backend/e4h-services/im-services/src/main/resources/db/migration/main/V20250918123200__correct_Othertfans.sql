1f3cb525e77fb3ba2a9965d7c954d7f1fe7a610a
tanmaytalele <tanmaynitin.talele@beehyv.com>
2025-09-18 14:26:47 +0530

    fix(im-services): add migration to correct spelling of issue subtype 'OtherFan' in audit logs table

--- backend/e4h-services/im-services/src/main/resources/db/migration/main/V20250918123200__correct_Othertfans.sql
+++ backend/e4h-services/im-services/src/main/resources/db/migration/main/V20250918123200__correct_Othertfans.sql
@@ -1,3 +1,12 @@
 UPDATE public.eg_incident_v2
 SET incidentsubtype = 'OtherFan'
 WHERE incidentsubtype = 'OthertFan';
+
+UPDATE eg_audit_logs
+SET keyvaluemap = jsonb_set(
+    keyvaluemap,
+    '{incidentSubType}',
+    '"OtherFan"',
+    false
+)
+WHERE keyvaluemap ->> 'incidentSubType' = 'OthertFan';
