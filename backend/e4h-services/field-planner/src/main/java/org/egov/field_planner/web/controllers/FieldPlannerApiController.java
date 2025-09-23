fe3a01886ca0cca98a98296709e7c24481a17e53
Babacar Ndiaye <babacar.ndiay93@gmail.com>
2025-09-23 18:13:46 +0000

    Update field plan activity API

--- backend/e4h-services/field-planner/src/main/java/org/egov/field_planner/web/controllers/FieldPlannerApiController.java
+++ backend/e4h-services/field-planner/src/main/java/org/egov/field_planner/web/controllers/FieldPlannerApiController.java
@@ -73,33 +73,6 @@ public class FieldPlannerApiController {
         return new ResponseEntity<FieldPlanResponse>(fieldPlanResponse, HttpStatus.OK);
     }
 
-//    @RequestMapping(value = "/_search", method = RequestMethod.POST)
-//    public ResponseEntity<FieldPlanResponse> searchfieldPlan(
-//            @ApiParam(value = "Details for the fieldPlan.", required = true) @Valid @RequestBody FieldPlanRequest request,
-//            @NotNull @Min(0) @Max(1000) @ApiParam(value = "Pagination - limit records in response", required = true) @Valid @RequestParam(value = "limit", required = true) Integer limit,
-//            @NotNull @Min(0) @ApiParam(value = "Pagination - offset from which records should be returned in response", required = true) @Valid @RequestParam(value = "offset", required = true) Integer offset,
-//            @NotNull @ApiParam(value = "Unique id for a tenant.", required = true) @Valid @RequestParam(value = "tenantId", required = true) String tenantId,
-//            @ApiParam(value = "epoch of the time since when the changes on the object should be picked up. Search results from this parameter should include both newly created fieldplan since this time as well as any modified objects since this time. This criterion is included to help polling clients to get the changes in system since a last time they synchronized with the platform. ") @Valid @RequestParam(value = "lastChangedSince", required = false) Long lastChangedSince,
-//            @ApiParam(value = "Used in search APIs to specify if (soft) deleted records should be included in search results.", defaultValue = "false") @Valid @RequestParam(value = "includeDeleted", required = false, defaultValue = "false") Boolean includeDeleted,
-//            @ApiParam(value = "Used in fieldplan search API to limit the search results to only those fieldPlans whose creation date is after the specified 'createdFrom' date", defaultValue = "false") @Valid @RequestParam(value = "createdFrom", required = false) Long createdFrom,
-//            @ApiParam(value = "Used in fieldplan search API to limit the search results to only those fieldPlans whose creation date is before the specified 'createdTo' date", defaultValue = "false") @Valid @RequestParam(value = "createdTo", required = false) Long createdTo
-//    ) {
-//        List<FieldPlan> fieldPlans = fieldPlannerService.searchFieldPlan(
-//                request,
-//                limit,
-//                offset,
-//                tenantId,
-//                includeDeleted,
-//                lastChangedSince,
-//                createdFrom,
-//                createdTo
-//        );
-//        ResponseInfo responseInfo = ResponseInfoFactory.createResponseInfo(request.getRequestInfo(), true);
-//        Integer count = fieldPlannerService.countAllFieldPlans(request, tenantId, lastChangedSince, includeDeleted, createdFrom, createdTo);
-//        FieldPlanResponse fieldPlanResponse = FieldPlanResponse.builder().responseInfo(responseInfo).fieldPlans(fieldPlans).totalCount(count).build();
-//        return new ResponseEntity<FieldPlanResponse>(fieldPlanResponse, HttpStatus.OK);
-//    }
-
     @RequestMapping(value = "/_search", method = RequestMethod.POST)
     public ResponseEntity<FieldPlanResponse> searchfieldPlanV2(
             @ApiParam(value = "Details for the fieldPlan.", required = true) @Valid @RequestBody FieldPlanSearchRequest request,
@@ -133,7 +106,7 @@ public class FieldPlannerApiController {
         return ResponseEntity.status(HttpStatus.ACCEPTED).body(response);
     }
 
-    @RequestMapping(value = "/facility/v1/bulk/_create", method = RequestMethod.POST)
+    @RequestMapping(value = "/facility/bulk/_create", method = RequestMethod.POST)
     public ResponseEntity<ResponseInfo> fieldPlanFacilityV1BulkCreatePost(@ApiParam(value = "Capture linkage of Field Plan and facility.", required = true) @Valid @RequestBody FieldPlanFacilityBulkRequest request) {
         request.getRequestInfo().setApiId(httpServletRequest.getRequestURI());
         producer.push(fieldPlannerConfiguration.getBulkCreateFieldPlanFacilityTopic(), request);
@@ -154,7 +127,7 @@ public class FieldPlannerApiController {
         return ResponseEntity.status(HttpStatus.ACCEPTED).body(response);
     }
 
-    @RequestMapping(value = "/facility/v1/bulk/_unassign", method = RequestMethod.POST)
+    @RequestMapping(value = "/facility/bulk/_unassign", method = RequestMethod.POST)
     public ResponseEntity<ResponseInfo> fieldPlanFacilityUnassignBulk(@ApiParam(value = "Capture linkage of Field Plan and facility.", required = true) @Valid @RequestBody FieldPlanFacilityBulkRequest request) {
         request.getRequestInfo().setApiId(httpServletRequest.getRequestURI());
         producer.push(fieldPlannerConfiguration.getBulkUnassignFieldPlanFacilityTopic(), request);
