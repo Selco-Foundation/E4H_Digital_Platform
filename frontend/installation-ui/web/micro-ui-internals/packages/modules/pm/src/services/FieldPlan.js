7bf33f146a4e813f27da1b78f8b4e749fd5d633b
adithyakatuku <adithya.beehyv@gmail.com>
2025-10-06 13:42:38 +0530

    [FEATURE]: Project Manager UI - Field Plan Creation (#1485)
    * Integrated field plans search api in project field plans page
    
    * Integrated field plan create and update api calls
    
    * Integrated field plan facility data apis
    
    * Added project id in payload for project facility data validation api call
    
    * Added activity details page UI
    
    * Activity details page removal options refactorings
    
    * Integrated MDMS, Organization apis into activity details
    
    * Integrated activity assign api
    
    * Integrated activity assignment search api
    
    * Added activity assignment update and delete apis
    
    * Added project status update api
    
    * Activity details page refactoring
    
    * CSSv1.0.7
    
    * Restricted edits to field plan data which is not in draft status
    
    * Integrated activity assignment edit api
    
    * Custom dropdown implementation and ui bug fixes
    
    * Code rabbit changes
    
    * CSSv1.0.8

---
+++ frontend/installation-ui/web/micro-ui-internals/packages/modules/pm/src/services/FieldPlan.js
@@ -0,0 +1,48 @@
+import {Request} from "@egovernments/digit-ui-libraries";
+
+export const FieldPlanService = {
+
+  fetchFieldPlans: async (queryFilter, limit, offset) => {
+    const endpoint = "/field-planner/v1/field-plans/_search";
+    const params = {
+      tenantId: "in",
+      offset,
+      limit,
+    }
+
+    const headers = {
+      "Content-Type": "application/json"
+    }
+
+    return await Request({
+      url: endpoint,
+      data: queryFilter,
+      userService: true,
+      method: "POST",
+      auth: true,
+      params: params,
+      headers: headers,
+    });
+  },
+
+  upsertFieldPlan: async (fieldPlanData) => {
+    let endpoint = "/field-planner/v1/field-plans/_create";
+    const headers = {
+      "Content-Type": "application/json"
+    }
+
+    if (fieldPlanData?.apiOperation === "UPDATE") {
+      endpoint = "/field-planner/v1/field-plans/_update";
+    }
+
+    return await Request({
+      url: endpoint,
+      data: fieldPlanData,
+      userService: true,
+      method: "POST",
+      auth: true,
+      headers: headers,
+    });
+  },
+
+}
--- frontend/installation-ui/web/micro-ui-internals/packages/modules/pm/src/services/PMService.js
+++ frontend/installation-ui/web/micro-ui-internals/packages/modules/pm/src/services/PMService.js
@@ -2,7 +2,7 @@ import { IngestionService } from "./Ingestion";
 
 export const PMService = {
 
-  projectFacilityDataTemplateDownload: async (projectId, boundaryData, t) => {
+  downloadProjectFacilityDataTemplate: async (projectId, boundaryData, t) => {
 
     const formatDistricts = (districts, blocks) => {
       const formattedDistricts = [];
@@ -46,13 +46,13 @@ export const PMService = {
       ]
     }
 
-    return await IngestionService.downloadTemplateFile({
+    return await IngestionService.downloadProjectFacilityDataTemplate({
       boundary_data: formattedBoundaryData,
       project_id: projectId,
     });
   },
 
-  projectFacilityDataTemplateUpload: async (file, projectId) => {
+  uploadProjectFacilityDataTemplate: async (file, projectId) => {
 
     const extractBlobFile = (response) => {
       const disposition = response.headers["content-disposition"];
@@ -73,7 +73,8 @@ export const PMService = {
     try {
       const validationRequest = new FormData();
       validationRequest.append("facility_file", file);
-      const validationResponse = await IngestionService.validateFacilityDataTemplate(validationRequest);
+      validationRequest.append("project_id", projectId);
+      const validationResponse = await IngestionService.validateProjectFacilityData(validationRequest);
 
       validatedFile = extractBlobFile(validationResponse);
       const errorCount = parseInt(validationResponse.headers["x-error-count"] || "0", 10);
@@ -88,7 +89,7 @@ export const PMService = {
     } catch (error) {
       console.error("Error validating facility data", error);
 
-      if (error.response.status === 400) {
+      if (error?.response?.status === 400) {
         return {
           errorCode: "INVALID_TEMPLATE",
         }
@@ -101,7 +102,7 @@ export const PMService = {
       const uploadRequest = new FormData();
       uploadRequest.append("facility_file", validatedFile.data);
       uploadRequest.append("project_id", projectId);
-      const uploadResponse = await IngestionService.uploadTemplateFile(uploadRequest)
+      const uploadResponse = await IngestionService.uploadProjectFacilityData(uploadRequest)
 
       const uploadedFile = extractBlobFile(uploadResponse);
       return {
@@ -112,5 +113,117 @@ export const PMService = {
       console.error("Error uploading facility data", error);
       throw error;
     }
-  }
+  },
+
+  downloadFieldPlanFacilityDataTemplate: async (fieldPlanId, boundaryData, t) => {
+
+    const formatDistricts = (districts, blocks) => {
+      const formattedDistricts = [];
+
+      districts.forEach((district) => {
+        const formattedDistrict = {
+          boundaryCode: district.code,
+          type: "district",
+          name: t(`DISTRICT_${district.code.toUpperCase()}`),
+          children: [
+            ...blocks
+              .filter((block) => block.districtCode === district.code)
+              .map((block) => ({
+                boundaryCode: block.code,
+                type: "block",
+                name: t(`BLOCK_${block.code.toUpperCase()}`),
+              }))
+          ]
+        }
+
+        formattedDistricts.push(formattedDistrict)
+      })
+
+      return formattedDistricts;
+    }
+
+    const formattedBoundaryData = {
+      boundaryCode: "India",
+      type: "country",
+      name: "India",
+      tenantId: "in",
+      children: [
+        {
+          boundaryCode: boundaryData.state.code,
+          type: "state",
+          name: t(`STATE_${boundaryData.state.code.toUpperCase()}`),
+          children: [
+            ...formatDistricts(boundaryData.districts, boundaryData.blocks)
+          ]
+        }
+      ]
+    }
+
+    return await IngestionService.downloadFieldPlanFacilityDataTemplate({
+      boundary_data: formattedBoundaryData,
+      field_plan_id: fieldPlanId,
+    });
+  },
+
+  uploadFieldPlanFacilityDataTemplate: async (file, fieldPlanId) => {
+
+    const extractBlobFile = (response) => {
+      const disposition = response.headers["content-disposition"];
+      const filename = disposition?.split("filename=")[1]?.replace(/"/g, "");
+
+      const blobData = new Blob([response.data], {
+        type: response.headers["content-type"],
+      });
+
+      return {
+        name: filename,
+        data: blobData,
+      }
+    }
+
+    let validatedFile;
+
+    try {
+      const validationRequest = new FormData();
+      validationRequest.append("facility_file", file);
+      const validationResponse = await IngestionService.validateFieldPlanFacilityData(validationRequest);
+
+      validatedFile = extractBlobFile(validationResponse);
+      const errorCount = parseInt(validationResponse.headers["x-error-count"] || "0", 10);
+      if (errorCount) {
+        return {
+          errorCode: "INVALID_DATA",
+          file: validatedFile,
+          errorCount: errorCount
+        };
+      }
+
+    } catch (error) {
+      console.error("Error validating facility data", error);
+
+      if (error?.response?.status === 400) {
+        return {
+          errorCode: "INVALID_TEMPLATE",
+        }
+      }
+
+      throw error;
+    }
+
+    try {
+      const uploadRequest = new FormData();
+      uploadRequest.append("facility_file", validatedFile.data);
+      uploadRequest.append("fieldplan_id", fieldPlanId);
+      const uploadResponse = await IngestionService.uploadFieldPlanFacilityData(uploadRequest)
+
+      const uploadedFile = extractBlobFile(uploadResponse);
+      return {
+        file: uploadedFile,
+      };
+
+    } catch (error) {
+      console.error("Error uploading facility data", error);
+      throw error;
+    }
+  },
 }
