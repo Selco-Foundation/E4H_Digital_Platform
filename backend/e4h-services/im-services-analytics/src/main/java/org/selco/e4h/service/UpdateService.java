9234aecf0f6f2d5285df5697b6af887feee1521b
tanmaytalele <tanmaynitin.talele@beehyv.com>
2025-10-10 16:08:10 +0530

    corrected the index name (#1504)

--- backend/e4h-services/im-services-analytics/src/main/java/org/selco/e4h/service/UpdateService.java
+++ backend/e4h-services/im-services-analytics/src/main/java/org/selco/e4h/service/UpdateService.java
@@ -181,7 +181,7 @@ public class UpdateService {
 			Map<String, Object> finalPayload = new HashMap<>();
 			finalPayload.put("Data", dataMap);
 
-			String indexUrl = config.getEsHostUrl() + "/computed-sla-im-services/_doc/" + documentId;
+			String indexUrl = config.getEsHostUrl() + "/computed-sla-im-services-write/_doc/" + documentId;
 			HttpEntity<Map<String, Object>> entity = new HttpEntity<>(finalPayload, buildHeaders());
 
 			restTemplate.put(indexUrl, entity);
