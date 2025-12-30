ca296e91a3f5ba0f0ea6d75057d35ecfd5b9d98b
teena.t <teena.t@beehyv.com>
2025-12-30 17:12:23 +0530

    fixed the alertsubtype and InverterRules

--- backend/e4h-services/rms-service/src/main/java/org/egov/rms/service/PayloadGenerator.java
+++ backend/e4h-services/rms-service/src/main/java/org/egov/rms/service/PayloadGenerator.java
@@ -112,7 +112,7 @@ public class PayloadGenerator {
             case HIGH_VOLTAGE:
                 return "VoltageInverter";
             case BURNT_DISCONNECTED:
-                return "Overcharge";
+                return "BurnedBattery";
             case DEEP_DISCHARGING:
                 return "DeepDischarge";
             case OVERCHARGING:
