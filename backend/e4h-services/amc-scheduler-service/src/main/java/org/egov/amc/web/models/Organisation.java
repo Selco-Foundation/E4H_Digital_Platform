f00eeab441bfb8da868a785f665e0a97fdc7e0e8
Babacar Ndiaye <babacar.ndiay93@gmail.com>
2026-01-07 11:25:29 +0000

    Fix issue on amc configuration search response

--- backend/e4h-services/amc-scheduler-service/src/main/java/org/egov/amc/web/models/Organisation.java
+++ backend/e4h-services/amc-scheduler-service/src/main/java/org/egov/amc/web/models/Organisation.java
@@ -11,7 +11,6 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.egov.common.contract.models.Address;
 import org.egov.common.contract.models.AuditDetails;
 import org.springframework.validation.annotation.Validated;
 
