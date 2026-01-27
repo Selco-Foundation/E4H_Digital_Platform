a780ea1e6cef9d2eac8942952f04e0c025a7873d
adithyakatuku <adithya.beehyv@gmail.com>
2026-01-27 18:18:15 +0530

    Organization table refactors

--- frontend/installation-ui/web/micro-ui-internals/packages/modules/org/src/components/OrganizationTable/OrganizationTableBase.js
+++ frontend/installation-ui/web/micro-ui-internals/packages/modules/org/src/components/OrganizationTable/OrganizationTableBase.js
@@ -94,7 +94,8 @@ const OrganizationTableBase = ({ orgType }) => {
   const columns = [
     { Header: t("ORG_NAME") || "Name", Cell: ({ row }) => GetCell((row && row.original && row.original.name) || "-") },
     { Header: t("ORG_CODE") || "Code", Cell: ({ row }) => GetCell((row && row.original && row.original.code) || "-") },
-    { Header: t("ORG_TYPE") || "Type", Cell: ({ row }) => GetCell((row && row.original && row.original.orgType) || "-") },
+    { Header: t("ORGANIZATION_SUB_TYPE") || "Sub Type", Cell: ({ row }) => GetCell((row && row.original && t(`ORGANIZATION_SUB_TYPE_${row.original.orgSubType}`)) || "-") },
+    { Header: t("ORGANIZATION_STATUS") || "Status", Cell: ({ row }) => GetCell((row && row.original && t(`ORGANIZATION_STATUS_${row.original.orgStatus}`)) || "-") },
     { Header: t("ORG_POC_NAME") || "POC Name", Cell: ({ row }) => GetCell((row && row.original && row.original.pocName) || "-") },
   ];
 
