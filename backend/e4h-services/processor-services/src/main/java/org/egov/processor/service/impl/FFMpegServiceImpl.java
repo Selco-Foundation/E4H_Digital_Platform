6cea08ed5cddf0ca7b7ef4da2637d79dde1be183
teena.t <teena.t@beehyv.com>
2026-01-28 19:06:46 +0530

    refactored the larger methods

--- backend/e4h-services/processor-services/src/main/java/org/egov/processor/service/impl/FFMpegServiceImpl.java
+++ backend/e4h-services/processor-services/src/main/java/org/egov/processor/service/impl/FFMpegServiceImpl.java
@@ -30,27 +30,57 @@ public class FFMpegServiceImpl implements FFMpegService {
             ProcessingContext context, String inputPath, Path outputPath, VideoQualitySettings videoQuality) {
         log.trace("Method invoked: processQuality, videoId: {}, quality: {}", context.getVideoId(), videoQuality.getLabel());
 
+        Path outputDirectory = createOutputDirectory(context, outputPath, videoQuality);
+        String playlistFile = buildPlaylistFilePath(outputDirectory);
+        String command = buildFfmpegCommand(inputPath, playlistFile, videoQuality);
+
+        executeFfmpegCommand(command, videoQuality);
+
+        String baseFileName = extractBaseFileName(outputDirectory);
+        log.info("Successfully processed quality level: {} for videoId: {}", videoQuality.getLabel(), context.getVideoId());
+
+        return baseFileName;
+    }
+
+    private Path createOutputDirectory(ProcessingContext context, Path outputPath, VideoQualitySettings videoQuality) {
         log.trace("Creating output directory for quality level");
         Path path = directoryUtil.createDirectory(String.format("%s/%s/hls/%s",
                 outputPath.toString(), context.getVideoId(), videoQuality.getLabel()));
         log.debug("Output directory created: {}", path);
+        return path;
+    }
 
-        String file = String.format("%s/playlist.m3u8", path);
+    private String buildPlaylistFilePath(Path outputDirectory) {
+        String file = String.format("%s/playlist.m3u8", outputDirectory);
+        log.trace("Playlist file path generated: {}", file);
+        return file;
+    }
+
+    private String buildFfmpegCommand(String inputPath, String playlistFile, VideoQualitySettings videoQuality) {
         log.trace("Generating FFmpeg command, isOriginal: {}", videoQuality.isOriginal());
-        String command = videoQuality.isOriginal()
-                ? fFmpegCommandGenerator.getBaseCommand(inputPath, file)
-                : fFmpegCommandGenerator.getOptimizedCommand(inputPath,
-                "veryfast", videoQuality.getCrf(), videoQuality.getResolution(), videoQuality.getAudioBitRate(), file);
+        if (videoQuality.isOriginal()) {
+            return fFmpegCommandGenerator.getBaseCommand(inputPath, playlistFile);
+        }
 
+        return fFmpegCommandGenerator.getOptimizedCommand(
+                inputPath,
+                "veryfast",
+                videoQuality.getCrf(),
+                videoQuality.getResolution(),
+                videoQuality.getAudioBitRate(),
+                playlistFile
+        );
+    }
+
+    private void executeFfmpegCommand(String command, VideoQualitySettings videoQuality) {
         log.info("Executing FFmpeg command for quality: {}", videoQuality.getLabel());
         log.debug("FFmpeg command: {}", command);
         ffMpegExecutor.executeCommand(command);
+    }
 
-        String baseFileName = path.toString().split("output")[1];
+    private String extractBaseFileName(Path outputDirectory) {
+        String baseFileName = outputDirectory.toString().split("output")[1];
         log.debug("Base filename extracted: {}", baseFileName);
-
-        log.info("Successfully processed quality level: {} for videoId: {}", videoQuality.getLabel(), context.getVideoId());
-
         return baseFileName;
     }
 }
