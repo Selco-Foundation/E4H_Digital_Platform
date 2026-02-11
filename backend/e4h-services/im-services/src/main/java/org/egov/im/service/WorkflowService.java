5931a6db4b603b30042327ca88b4a64f20747438
Babacar Ndiaye <babacar.ndiay93@gmail.com>
2026-02-11 15:35:18 +0000

    Assign Submit action for PENDING_REVISION state to complaint facilitator 2 automatically Assign MARK_OUT_OF_SCOPE action for PENDINGRESOLUTION state to complaint facilitator 1 automatically

--- backend/e4h-services/im-services/src/main/java/org/egov/im/service/WorkflowService.java
+++ backend/e4h-services/im-services/src/main/java/org/egov/im/service/WorkflowService.java
@@ -277,6 +277,10 @@ public class WorkflowService {
             reassignWorkflow(workflow, request, "COMPLAINANT");
         } else if (action.equalsIgnoreCase("OUT_OF_WARRANTY")) {
             reassignWorkflow(workflow, request, "COMPLAINT_FACILITATOR_2");
+        } else if (request.getIncident().getApplicationStatus().trim().equals("PENDING_REVISION") && action.equalsIgnoreCase("SUBMIT")) {
+            reassignWorkflow(workflow, request, "COMPLAINT_FACILITATOR_2");
+        } else if (request.getIncident().getApplicationStatus().trim().equals("PENDINGRESOLUTION") && action.equalsIgnoreCase("MARK_OUT_OF_SCOPE")) {
+            reassignWorkflow(workflow, request, "COMPLAINT_FACILITATOR_1");
         }
         ProcessInstance processInstance = new ProcessInstance();
         processInstance.setBusinessId(incident.getIncidentId());
