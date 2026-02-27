21a66586fba6d9a9b1eafacf4c0c5f379042f3ba
Babacar Ndiaye <babacar.ndiay93@gmail.com>
2026-02-27 12:09:06 +0000

    Fix sonarqube issue

--- backend/core-services/egov-workflow-v2/src/main/java/org/egov/wf/Main.java
+++ backend/core-services/egov-workflow-v2/src/main/java/org/egov/wf/Main.java
@@ -93,7 +93,7 @@ public class Main {
                 }
             });
         } catch (Exception ex) {
-            ex.printStackTrace();
+//            ex.printStackTrace();
         }
     }
 
