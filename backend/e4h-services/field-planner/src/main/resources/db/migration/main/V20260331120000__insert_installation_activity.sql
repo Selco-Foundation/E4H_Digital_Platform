09a02934f793991a29bff8b644460cff32ac3304
babstar93 <140029411+babstar93@users.noreply.github.com>
2026-03-31 10:35:13 +0000

    Add migration file for adding default installation activity (#2261)
    Co-authored-by: Babacar Ndiaye <babacar.ndiay93@gmail.com>
    Co-authored-by: Teena Thakur <92431551+teenathakur01@users.noreply.github.com>

---
+++ backend/e4h-services/field-planner/src/main/resources/db/migration/main/V20260331120000__insert_installation_activity.sql
@@ -0,0 +1,38 @@
+INSERT INTO public.activities
+(
+  id,
+  tenant_id,
+  "name",
+  code,
+  default_conditions,
+  required_roles,
+  sequence_order,
+  is_active,
+  created_time,
+  last_modified_time,
+  additional_details
+)
+VALUES
+(
+  '272e7e4a-e02f-48ac-966d-1c920d88297c',
+  'in',
+  'Installation',
+  'INS',
+  '{}'::jsonb,
+  '{"roles": [{"code": "INSTALLATION_SPOC", "name": "Installation SPOC", "tenantId": "in", "description": ""}, {"code": "INSTALLATION_REVIEWER", "name": "Installation Reviewer", "tenantId": "in", "description": ""}]}'::jsonb,
+  1,
+  true,
+  1759402867975,
+  1759402867975,
+  'null'::jsonb
+)
+ON CONFLICT (tenant_id, code) DO UPDATE
+SET
+  "name" = EXCLUDED."name",
+  default_conditions = EXCLUDED.default_conditions,
+  required_roles = EXCLUDED.required_roles,
+  sequence_order = EXCLUDED.sequence_order,
+  is_active = EXCLUDED.is_active,
+  last_modified_time = EXCLUDED.last_modified_time,
+  additional_details = EXCLUDED.additional_details;
+
