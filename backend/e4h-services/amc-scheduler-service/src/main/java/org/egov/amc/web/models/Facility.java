001273c3800d8b4084e6a52ba52354007d14e640
babstar93 <140029411+babstar93@users.noreply.github.com>
2026-04-24 11:39:41 +0000

    Send sms amc otp, Allow search visit by facility name, add sorting (#2346)
    * Implement sms sending for AMC visit OTP
    
    * Implement sms sending for AMC visit OTP
    
    * Implement sms sending for AMC visit OTP
    
    * Implement sms sending for AMC visit OTP
    
    * Implement sms sending for AMC visit OTP
    
    * Implement sms sending for AMC visit OTP
    
    * Implement sms sending for AMC visit OTP
    
    * Add facility name when creating scheduled visit
    Add facilityName search criteria for Scheduled visit
    Add sort direction for scheduled visit
    Fix issue on assignedUsers search criteria
    
    ---------
    
    Co-authored-by: Babacar Ndiaye <babacar.ndiay93@gmail.com>
    Co-authored-by: Teena Thakur <92431551+teenathakur01@users.noreply.github.com>

--- backend/e4h-services/amc-scheduler-service/src/main/java/org/egov/amc/web/models/Facility.java
+++ backend/e4h-services/amc-scheduler-service/src/main/java/org/egov/amc/web/models/Facility.java
@@ -62,4 +62,25 @@ public class Facility {
 
     @JsonProperty("boundaryCode")
     private String boundaryCode = null;
+
+    @JsonProperty("isOnmReady")
+    private Boolean isOnmReady = false;
+
+    @JsonProperty("facility_poc_name")
+    private String facilityPocName = null;
+
+    @JsonProperty("facility_poc_phone")
+    private String facilityPocPhone = null;
+
+    @JsonProperty("facility_poc_email")
+    private String facilityPocEmail = null;
+
+    @JsonProperty("facility_status")
+    private String facilityStatus = null;
+
+    @JsonProperty("hfr_id")
+    private String hfrId = null;
+
+    @JsonProperty("nin_id")
+    private String ninId = null;
 }
