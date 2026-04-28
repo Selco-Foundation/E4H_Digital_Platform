9224eeabde4e4ac03b5364a579f48e54a882846d
teena.t <teena.t@beehyv.com>
2026-04-28 18:07:03 +0530

    added new modals

---
+++ backend/e4h-services/rms-service/src/main/java/org/egov/rms/model/TicketPausePayload.java
@@ -0,0 +1,34 @@
+package org.egov.rms.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.Instant;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class TicketPausePayload {
+
+    @JsonProperty("action")
+    private TicketPauseManageRequest.Action action;
+
+    @JsonProperty("facilityId")
+    private String facilityId;
+
+    @JsonProperty("facilityName")
+    private String facilityName;
+
+    @JsonProperty("boundaryCode")
+    private String boundaryCode;
+
+    @JsonProperty("pausedUntil")
+    private Instant pausedUntil;
+
+    @JsonProperty("reason")
+    private String reason;
+}
