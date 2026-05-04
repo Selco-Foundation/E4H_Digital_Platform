66c74291bdad05057925bfe99310dfc4a6cd2314
jogapps <jogappsdeveloper@gmail.com>
2026-05-04 08:42:01 +0100

    modified role selection page UI

--- mobile/lib/pages/role_selection.dart
+++ mobile/lib/pages/role_selection.dart
@@ -1,9 +1,7 @@
 import 'package:digit_ui_components/digit_components.dart';
 import 'package:digit_ui_components/theme/digit_extended_theme.dart';
-import 'package:digit_ui_components/widgets/molecules/digit_card.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:selco/utils/asset_images.dart';
 
 import '../blocs/auth/authbloc.dart';
 import '../blocs/role_selection/role_selection.dart';
@@ -11,8 +9,8 @@ import '../blocs/user_type/user_type.dart';
 import '../model/response/responsemodel.dart';
 import '../router/app_router.dart';
 import '../utils/extensions.dart';
-import '../utils/i18_key_constants.dart' as i18;
 import '../utils/role_login_resolver.dart';
+import '../widgets/home/home_item_card.dart';
 
 @RoutePage()
 class RoleSelectionPage extends StatefulWidget {
@@ -42,98 +40,73 @@ class _RoleSelectionView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final screenWidth = context.width;
+
     final theme = Theme.of(context);
-    final textTheme = theme.digitTextTheme(context);
     return Scaffold(
-      body: ScrollableContent(
-        enableFixedDigitButton: true,
-        backgroundColor: theme.colorScheme.secondary,
-        footer: BlocBuilder<RoleSelectionBloc, RoleSelectionState>(
-          builder: (context, state) {
-            return DigitCard(
-              margin: const EdgeInsets.only(top: spacer2),
-              cardType: CardType.secondary,
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: state.availableRoles.map((role) {
-                    final isSelected = state.selectedRole == role;
-                    return Expanded(
-                      child: Padding(
-                        padding: const EdgeInsets.symmetric(
-                          horizontal: spacer1 / 2,
-                        ),
-                        child: DigitButton(
-                          label: role.label,
+      backgroundColor: DigitTheme.instance.colorScheme.surface,
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: spacer2),
+        child: ScrollableContent(
+          backgroundColor: theme.colorTheme.generic.background,
+          footer: const PoweredByDigit(version: ''),
+          slivers: [
+            BlocBuilder<RoleSelectionBloc, RoleSelectionState>(
+              builder: (context, state) {
+                return SliverPadding(
+                  padding: const EdgeInsets.only(top: spacer6),
+                  sliver: SliverGrid(
+                    delegate: SliverChildBuilderDelegate(
+                      (context, index) {
+                        final role = state.availableRoles[index];
+                        return HomeItemCard(
+                          icon: _iconForRole(role),
+                          label: role.isAmc ? role.label : "Installation",
                           onPressed: () {
                             context.read<RoleSelectionBloc>().add(
                                   RoleSelectionRoleSelected(role),
                                 );
+                            context.read<UserTypeBloc>().add(
+                                  UserTypeEvent.typeSelected(
+                                    role.userTypeValue,
+                                  ),
+                                );
+
+                            context.router.replace(
+                              role.isAmc
+                                  ? const AmcHomeRoute()
+                                  : const HomeRoute(),
+                            );
                           },
-                          type: isSelected
-                              ? DigitButtonType.primary
-                              : DigitButtonType.secondary,
-                          size: DigitButtonSize.large,
-                        ),
-                      ),
-                    );
-                  }).toList(),
-                ),
-                SizedBox(
-                  width: context.width,
-                  child: DigitButton(
-                    label: context.translate(i18.common.coreCommonProceed),
-                    isDisabled: !state.canProceed,
-                    onPressed: () {
-                      final selectedRole = state.selectedRole;
-                      if (selectedRole == null) return;
-
-                      context.read<UserTypeBloc>().add(
-                            UserTypeEvent.typeSelected(
-                              selectedRole.userTypeValue,
-                            ),
-                          );
-
-                      context.router.replace(
-                        selectedRole.isAmc
-                            ? const AmcHomeRoute()
-                            : const HomeRoute(),
-                      );
-                    },
-                    type: DigitButtonType.primary,
-                    size: DigitButtonSize.large,
-                  ),
-                ),
-              ],
-            );
-          },
-        ),
-        children: [
-          SizedBox(
-            height: context.height * 0.6,
-            child: Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Image.asset(
-                    color: theme.colorTheme.paper.primary,
-                    AssetImages.APP_LOGO,
-                    height: spacer12 * 2,
-                    width: spacer12 * 2,
-                  ),
-                  Text(
-                    "Select Role",
-                    style: textTheme.headingM.copyWith(
-                      color: theme.colorTheme.primary.primary1,
+                        );
+                      },
+                      childCount: state.availableRoles.length,
                     ),
-                  )
-                ],
-              ),
-            ),
-          ),
-        ],
+                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2,
+                      mainAxisSpacing: spacer4,
+                      childAspectRatio:
+                          (screenWidth / 2) / (170 * (screenWidth / 375)),
+                    ),
+                  ),
+                );
+              },
+            )
+          ],
+          children: const [],
+        ),
       ),
     );
+  }
+
+  IconData _iconForRole(RoleSelectionOption role) {
+    switch (role) {
+      case RoleSelectionOption.staff:
+        return Icons.person_outline;
+      case RoleSelectionOption.supervisor:
+        return Icons.supervisor_account_outlined;
+      case RoleSelectionOption.amc:
+        return Icons.home_repair_service_outlined;
+    }
   }
 }
