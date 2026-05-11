069ea2808ee93b734357b01ae1426cf2d3c3ed23
Babacar Ndiaye <babacar.ndiay93@gmail.com>
2026-05-11 10:32:45 +0000

    Fix issue when updating old ticket status

--- backend/e4h-services/im-services/src/main/java/org/egov/im/service/UserService.java
+++ backend/e4h-services/im-services/src/main/java/org/egov/im/service/UserService.java
@@ -147,7 +147,6 @@ public class UserService {
         log.trace("UserService::enrichUser method invoked");
         RequestInfo requestInfo = request.getRequestInfo();
         String accountId = request.getIncident().getReporter().getUuid();
-//        String tenantId = request.getIncident().getPhcType();
 
         log.trace("Searching user by accountId");
         UserDetailResponse userDetailResponse = searchUser(null,accountId,null);
@@ -217,7 +216,6 @@ public class UserService {
         UserSearchRequest userSearchRequest =new UserSearchRequest();
         userSearchRequest.setActive(true);
         userSearchRequest.setUserType(USERTYPE_EMPLOYEE);
-//        userSearchRequest.setTenantId(stateLevelTenant);
 
         if(StringUtils.isEmpty(accountId) && StringUtils.isEmpty(userName))
             return null;
