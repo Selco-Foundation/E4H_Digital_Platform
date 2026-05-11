8c0e3ef26d159424a58c2ea647c3c5ed1be223a0
adithyakatuku <adithya.beehyv@gmail.com>
2026-05-11 18:39:12 +0530

    Fixed Tech POC user role check (#2402)

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/im/src/pages/employee/Inbox.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/im/src/pages/employee/Inbox.js
@@ -12,7 +12,7 @@ const Inbox = () => {
   const { userName, roles } = Digit.UserService.getUser().info;
   const [totalRecords, setTotalRecords] = useState(0);
   const { nearing } = Digit.Hooks.useQueryParams();
-  const isTechPocUser = roles?.every((role) => role.code === "COMPLAINT_FACILITATOR_2" || role.code === "EMPLOYEE");
+  const isTechPocUser = roles?.some((role) => role.code === "COMPLAINT_FACILITATOR_2");
 
   const isCodePresent = (array, codeToCheck) => array.some((item) => item.code === codeToCheck);
 
