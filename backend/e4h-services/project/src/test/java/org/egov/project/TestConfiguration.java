f3d9e33438247bea4959ea748472c20f74416231
teena.t <teena.t@beehyv.com>
2026-05-20 22:38:50 +0530

    fetch the projectIds and projectNames given a list of facility IDs

--- backend/e4h-services/project/src/test/java/org/egov/project/TestConfiguration.java
+++ backend/e4h-services/project/src/test/java/org/egov/project/TestConfiguration.java
@@ -1,5 +1,6 @@
 package org.egov.project;
 
+import org.egov.project.service.Co2ProjectLookupService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.KafkaTemplate;
@@ -12,5 +13,10 @@ public class TestConfiguration {
     @SuppressWarnings("unchecked")
     public KafkaTemplate<String, Object> kafkaTemplate() {
         return mock(KafkaTemplate.class);
+    }
+
+    @Bean
+    public Co2ProjectLookupService co2ProjectLookupService() {
+        return mock(Co2ProjectLookupService.class);
     }
 }
