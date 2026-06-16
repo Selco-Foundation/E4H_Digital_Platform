9f95a58e6ad85278f166880a68b94837e3c7d35f
adithyakatuku <adithya.beehyv@gmail.com>
2026-06-16 13:55:43 +0530

    Added Installation Completion Certificate and Asset Handover Document to Installation Reviewer UI (#2591)
    * Added Installation Completion Certificate and Asset Handover Document to Installation Reviewer UI (#2589) (#2590)
    
    * Add poc number on search activity response endpoint
    
    ---------
    
    Co-authored-by: Babacar Ndiaye <babacar.ndiay93@gmail.com>

--- frontend/installation-ui/web/micro-ui-internals/packages/modules/qc/src/components/FacilityDetails/SystemParameterReport.js
+++ frontend/installation-ui/web/micro-ui-internals/packages/modules/qc/src/components/FacilityDetails/SystemParameterReport.js
@@ -3,20 +3,82 @@ import { PdfIcon } from "@egovernments/digit-ui-svg-components";
 import { ImageViewer } from "@egovernments/digit-ui-react-components";
 import CustomFileIcon from "../Custom/CustomFileIcon";
 
-const SystemParameterReport = ({ t, file, supportingDocuments, installationImages }) => {
+const SystemParameterReport = ({ t, file, supportingDocuments, installationImages, installationCompletionCertificate, assetHandoverDocument }) => {
 
   const [imageToView, setImageToView] = useState(null);
 
+  const installationCompletionCertificatePresent = installationCompletionCertificate.length > 0;
+  const assetHandoverDocumentPresent = assetHandoverDocument.length > 0;
   const supportingDocumentsPresent = supportingDocuments.length > 0;
   const installationImagesPresent = installationImages.some(({ images }) => images.length > 0);
+
+  const isNotLastSection = (index) => {
+    if (index === 1) return installationCompletionCertificatePresent || assetHandoverDocumentPresent || supportingDocumentsPresent || installationImagesPresent;
+    if (index === 2) return assetHandoverDocumentPresent || supportingDocumentsPresent || installationImagesPresent;
+    if (index === 3) return supportingDocumentsPresent || installationImagesPresent;
+    if (index === 4) return installationImagesPresent;
+  }
+
+  const AdditionalDocuments = ({ index, heading, additionalDocuments }) => (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        gap: "20px",
+        paddingBottom: isNotLastSection(index) ? "20px" : "0",
+        marginBottom: isNotLastSection(index) ? "20px" : "0",
+        borderBottom: isNotLastSection(index) ? "1px solid #D6D5D4" : "0",
+      }}
+    >
+      <div
+        style={{
+          color: "#0B3954",
+          fontSize: "20px",
+        }}
+      >
+        {heading}
+      </div>
+      {additionalDocuments?.map((additionalDocument) => (
+        <div
+          style={{
+            border: "1px solid #eee",
+            borderRadius: "6px",
+            padding: "16px",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "space-between",
+            minWidth: "fit-content",
+            width: "20%",
+            position: "relative",
+          }}
+        >
+          <a
+            style={{ textDecoration: "none", color: "unset" }}
+            target="_blank"
+            rel="noopener noreferrer"
+            href={additionalDocument.fileUrl}
+            download={additionalDocument.name || "supporting-doc"}
+          >
+            <div style={{ display: "flex", alignItems: "center", gap: "12px" }}>
+              <CustomFileIcon fileName={additionalDocument.name || additionalDocument.fileType} />
+              <div>
+                <div style={{ fontWeight: "bold", fontSize: "16px" }}>{additionalDocument.name}</div>
+                {additionalDocument?.size && <div style={{ color: "#666", fontSize: "14px" }}>{additionalDocument.size}</div>}
+              </div>
+            </div>
+          </a>
+        </div>
+      ))}
+    </div>
+  );
 
   return (
     <div style={{ padding: "20px" }}>
       <div
         style={{
-          paddingBottom: supportingDocumentsPresent || installationImagesPresent ? "20px" : "0",
-          marginBottom: supportingDocumentsPresent || installationImagesPresent ? "20px" : "0",
-          borderBottom: supportingDocumentsPresent || installationImagesPresent ? "1px solid #D6D5D4" : "0",
+          paddingBottom: isNotLastSection(1) ? "20px" : "0",
+          marginBottom: isNotLastSection(1) ? "20px" : "0",
+          borderBottom: isNotLastSection(1) ? "1px solid #D6D5D4" : "0",
         }}
       >
         <div
@@ -49,58 +111,9 @@ const SystemParameterReport = ({ t, file, supportingDocuments, installationImage
           </a>
         </div>
       </div>
-      {supportingDocumentsPresent && (
-        <div
-          style={{
-            display: "flex",
-            flexDirection: "column",
-            gap: "20px",
-            paddingBottom: installationImagesPresent ? "20px" : "0",
-            marginBottom: installationImagesPresent ? "20px" : "0",
-            borderBottom: installationImagesPresent ? "1px solid #D6D5D4" : "0",
-          }}
-        >
-          <div
-            style={{
-              color: "#0B3954",
-              fontSize: "20px",
-            }}
-          >
-            {t("SUPPORTING_DOCUMENTS")}
-          </div>
-          {supportingDocuments?.map((supportingDocument) => (
-            <div
-              style={{
-                border: "1px solid #eee",
-                borderRadius: "6px",
-                padding: "16px",
-                display: "flex",
-                alignItems: "center",
-                justifyContent: "space-between",
-                minWidth: "fit-content",
-                width: "20%",
-                position: "relative",
-              }}
-            >
-              <a
-                style={{ textDecoration: "none", color: "unset" }}
-                target="_blank"
-                rel="noopener noreferrer"
-                href={supportingDocument.fileUrl}
-                download={supportingDocument.name || "supporting-doc"}
-              >
-                <div style={{ display: "flex", alignItems: "center", gap: "12px" }}>
-                  <CustomFileIcon fileName={supportingDocument.name || supportingDocument.fileType} />
-                  <div>
-                    <div style={{ fontWeight: "bold", fontSize: "16px" }}>{supportingDocument.name}</div>
-                    {supportingDocument?.size && <div style={{ color: "#666", fontSize: "14px" }}>{supportingDocument.size}</div>}
-                  </div>
-                </div>
-              </a>
-            </div>
-          ))}
-        </div>
-      )}
+      {installationCompletionCertificatePresent && <AdditionalDocuments index={2} heading={t("INSTALLATION_COMPLETION_CERTIFICATE")} additionalDocuments={installationCompletionCertificate} />}
+      {assetHandoverDocumentPresent && <AdditionalDocuments index={3} heading={t("ASSET_HANDOVER_DOCUMENT")} additionalDocuments={assetHandoverDocument} />}
+      {supportingDocumentsPresent && <AdditionalDocuments index={4} heading={t("SUPPORTING_DOCUMENTS")} additionalDocuments={supportingDocuments} />}
       {installationImagesPresent && (
         <div
           style={{
