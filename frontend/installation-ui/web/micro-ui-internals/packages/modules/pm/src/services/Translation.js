12a5ce120ec188bfede46be4ef37ac649d39d1d8
revanth.korra <revanth.korra@beehyv.com>
2026-07-15 18:01:41 +0530

    feat:translation test work

---
+++ frontend/installation-ui/web/micro-ui-internals/packages/modules/pm/src/services/Translation.js
@@ -0,0 +1,16 @@
+import { CustomRequest } from "../components/Custom/CustomRequest";
+
+export const TranslationService = {
+  translateExcel: async (translationRequest) => {
+    return await CustomRequest({
+      url: "/translate/excel",
+      data: translationRequest,
+      method: "POST",
+      auth: true,
+      noRequestInfo: true,
+      fileDownload: true,
+      responseType: "blob",
+      defaultFilename: "translated-file.xlsx",
+    });
+  },
+};
