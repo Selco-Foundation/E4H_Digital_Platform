e26682f541e4feafd1316de4f57b1afb9378d406
revanth.korra <revanth.korra@beehyv.com>
2026-07-31 13:38:36 +0530

    fix:fixed inverter key

--- frontend/installation-ui/web/micro-ui-internals/packages/modules/qc/src/components/FacilityDetails/Summary.js
+++ frontend/installation-ui/web/micro-ui-internals/packages/modules/qc/src/components/FacilityDetails/Summary.js
@@ -195,7 +195,7 @@ const Summary = ({
         ) : (
           <div style={{ padding: "20px" }}>
             <Section title={t(`QC_INSTALLATION_ASSET_COUNT`)}>
-              {AssetInfoItem(t("QC_INSTALLATION_INVERTER") , count)}
+              {AssetInfoItem(t(`QC_INSTALLATION_${section}`), count)}
             </Section>
 
             <Section title={t(`QC_INSTALLATION_${section}_SPECIFICATIONS`)}>
