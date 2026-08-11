cd10d766d7d9cf571ea46574636a80c86afa9059
adithyakatuku <adithya.beehyv@gmail.com>
2026-08-11 14:46:14 +0530

    Revert "Fixed updating assessment plan response page reading 'Assessment Plan Created'"
    This reverts commit 7291b2459e087aef264df4f8a1b267a6171198b7.

--- frontend/installation-ui/web/micro-ui-internals/packages/modules/pm/src/pages/employee/CreateAssessment.js
+++ frontend/installation-ui/web/micro-ui-internals/packages/modules/pm/src/pages/employee/CreateAssessment.js
@@ -440,11 +440,11 @@ const CreateAssessment = () => {
     return assessors;
   }
 
-  const assignAssessmentActivityUsers = async (activityData) => {
+  const assignAssessmentActivityUsers = async (activityData, planOverrides = {}) => {
     const assessors = formatAssessorsForUpdate(activityData);
 
     const assessmentPlanUpdateData = {
-      AssessmentPlans: [{ ...createdAssessmentPlan }],
+      AssessmentPlans: [{ ...createdAssessmentPlan, ...planOverrides }],
       assessors,
       apiOperation: "UPDATE",
     };
@@ -790,17 +790,17 @@ const CreateAssessment = () => {
 
     } else {
       setBlockUI(true);
-      const updatingAssessmentPlan = !!createdAssessmentPlan?.id;
+      const schedulingAssessmentPlan = createdAssessmentPlan?.status === "DRAFT";
 
       try {
-        await assignAssessmentActivityUsers(activityData);
+        await assignAssessmentActivityUsers(activityData, schedulingAssessmentPlan ? { status: "SCHEDULED" } : {});
 
         const upsertedAssessmentPlanData = await invalidateAssessmentPlanData();
         const upsertedAssessmentPlan = upsertedAssessmentPlanData?.assessmentPlans?.[0];
         dispatch(
           populateResponsePage({
             response: {},
-            message: updatingAssessmentPlan ? t("PM_COMMON_ASSESSMENT_PLAN_UPDATED") : t("PM_COMMON_ASSESSMENT_PLAN_CREATED"),
+            message: schedulingAssessmentPlan ? t("PM_COMMON_ASSESSMENT_PLAN_CREATED") : t("PM_COMMON_ASSESSMENT_PLAN_UPDATED"),
             createdId: upsertedAssessmentPlan?.name,
             info: t("PM_COMMON_ASSESSMENT_PLAN_NAME"),
             secondaryRedirectionLabel: t("PM_LABEL_GO_TO_PROJECT"),
@@ -813,7 +813,7 @@ const CreateAssessment = () => {
         console.error("Error submitting assessment plan creation form", error);
         setToast({
           key: "error",
-          label: CommonUtils.getApiErrorMessage(error) || (updatingAssessmentPlan ? t("PM_TOAST_ASSESSMENT_PLAN_SUBMIT_CREATE_ERROR") : t("PM_TOAST_ASSESSMENT_PLAN_SUBMIT_UPDATE_ERROR")),
+          label: CommonUtils.getApiErrorMessage(error) || (schedulingAssessmentPlan ? t("PM_TOAST_ASSESSMENT_PLAN_SUBMIT_CREATE_ERROR") : t("PM_TOAST_ASSESSMENT_PLAN_SUBMIT_UPDATE_ERROR")),
         })
 
       } finally {
