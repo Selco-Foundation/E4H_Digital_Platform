bfea0a26fddc8879c5564cf9b442a03a869504a7
Pasam <148050850+Pasam6@users.noreply.github.com>
2026-08-18 10:36:56 +0530

    Revert "Rename installation qc ctx uat" (#2933)
    * Revert "update fallback context path"
    
    This reverts commit 231728517aa59c8626b412076068535428cc3c11.
    
    * Revert "update public path"
    
    This reverts commit e5a5a6c7a290b3b58e17923592b25e78767a77a8.

--- frontend/installation-ui/web/webpack.config.js
+++ frontend/installation-ui/web/webpack.config.js
@@ -31,7 +31,7 @@ module.exports = {
   output: {
     filename: "[name].bundle.js",
     path: path.resolve(__dirname, "build"),
-    publicPath: "/e4hhub/",
+    publicPath: "/installation-qc/",
   },
   optimization: {
     splitChunks: {
