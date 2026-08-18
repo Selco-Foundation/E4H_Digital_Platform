da54dfd41e2b10f3e28286ac45efe7e3cd4f0e0a
Babacar Ndiaye <babacar.ndiay93@gmail.com>
2026-08-18 12:15:56 +0000

    Replace HFR/NIN with username when facility type is ANGANWADI on pdf report

--- backend/e4h-services/amc-scheduler-service/src/main/java/org/egov/amc/service/AmcVisitReportPdfService.java
+++ backend/e4h-services/amc-scheduler-service/src/main/java/org/egov/amc/service/AmcVisitReportPdfService.java
@@ -138,7 +138,7 @@ public class AmcVisitReportPdfService {
                 ? boundaryLocalizationUtil.localizedNameOrCode(boundaryNames, boundary.getDistrict()) : null);
         data.put("project_block", boundary != null
                 ? boundaryLocalizationUtil.localizedNameOrCode(boundaryNames, boundary.getBlock()) : null);
-        data.put("nin_id", facility.getNinId() != null ? facility.getNinId() : facility.getHfrId());
+        data.put("nin_id", facility.getNinId() != null ? facility.getNinId() : (facility.getHfrId() != null ? facility.getHfrId() : facility.getFacilityPocUsername()));
         data.put("po_wo_number", null);
         data.put("documents", buildDocumentsForPdf(visitReport));
 
