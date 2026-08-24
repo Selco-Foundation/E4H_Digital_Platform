c6e3405d3237079f851d1c97369f1f69d765fbec
revanth.korra <revanth.korra@beehyv.com>
2026-08-24 13:15:44 +0530

    fix:changed terms and privacy folder structure

--- frontend/installation-ui/web/micro-ui-internals/packages/modules/core/src/pages/employee/TermsPrivacyPolicy/index.js
+++ frontend/installation-ui/web/micro-ui-internals/packages/modules/core/src/pages/employee/TermsPrivacyPolicy/index.js
@@ -2,8 +2,8 @@ import React from "react";
 import { useTranslation } from "react-i18next";
 import { useHistory } from "react-router-dom";
 import { HeaderComponent, Loader } from "@egovernments/digit-ui-components";
-import PolicyDocumentContent from "../../components/PolicyDocumentContent";
-import usePolicyDocument from "../../hooks/usePolicyDocument";
+import PolicyDocumentContent from "../../../components/PolicyDocumentContent";
+import usePolicyDocument from "../../../hooks/usePolicyDocument";
 
 const POLICY_CONFIG = {
   privacy: {
