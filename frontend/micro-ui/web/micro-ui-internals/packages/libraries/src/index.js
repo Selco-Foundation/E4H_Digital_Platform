43d036186819c0afa22ad6fa1ead712e51851e38
krevanth5299 <revanth.korra@beehyv.com>
2026-08-24 16:47:15 +0530

    Fix:Removed duplicate request import (#2970)

--- frontend/micro-ui/web/micro-ui-internals/packages/libraries/src/index.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/libraries/src/index.js
@@ -3,12 +3,10 @@ import Enums from "./enums/index";
 import mergeConfig from "./config/mergeConfig";
 import { useStore } from "./services/index";
 import { initI18n } from "./translations/index";
-import { Request } from "./services/atoms/Utils/Request";
 import { Storage, PersistantStorage } from "./services/atoms/Utils/Storage";
 import { Request } from "./services/atoms/Utils/Request";
 import { UploadServices } from "./services/atoms/UploadServices";
 import JsDictionary from "./services/atoms/JsDictionary";
-
 import { LocationService } from "./services/elements/Location";
 import { LocalityService } from "./services/elements/Localities";
 import { CustomService } from "./services/elements/CustomService";
