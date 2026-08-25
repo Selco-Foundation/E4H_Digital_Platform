1ac4d00f47a1fcceadbb0dad0cd21a0102f12b12
revanth.korra <revanth.korra@beehyv.com>
2026-08-25 15:18:12 +0530

    fix:created AssetImageViewer file

--- frontend/installation-ui/web/micro-ui-internals/packages/modules/qc/src/components/FacilityDetails/Summary.js
+++ frontend/installation-ui/web/micro-ui-internals/packages/modules/qc/src/components/FacilityDetails/Summary.js
@@ -3,62 +3,11 @@ import Section from "./Section";
 import AddRejectionReasonModal from "./AddRejectionReasonModal";
 import SystemParameterReport from "./SystemParameterReport";
 import EditRejectionReasonModal from "./EditRejectionReasonModal";
+import AssetImageViewer from "./AssetImageViewer";
 import { useDispatch, useSelector } from "react-redux";
 import { setRejectionReasons } from "../../redux/actions";
 import { ImageViewer, Loader } from "@egovernments/digit-ui-react-components";
 import CustomCloseSvg from "../CustomCloseSvg";
-
-const AssetImageViewer = ({ t, image, onClose }) => {
-  return (
-    <div
-      className="image-viewer-wrap"
-      style={{
-        paddingTop: "100px",
-        paddingBottom: "100px",
-        overflow: "auto",
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        justifyContent: "flex-start",
-        gap: "16px"
-      }}
-    >
-      <CustomCloseSvg onClick={onClose} fill="none" iconFill="white" />
-      <div
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "stretch",
-          gap: "16px",
-          width: "100%",
-          maxWidth: "700px",
-          boxSizing: "border-box"
-        }}
-      >
-        <img src={image?.src} style={{ margin: 0, width: "100%", maxWidth: "100%", height: "auto", display: "block" }} />
-        <div
-          style={{
-            background: "white",
-            color: "#0B0C0C",
-            padding: "12px 16px",
-            borderRadius: "4px",
-            boxSizing: "border-box",
-            width: "100%"
-          }}
-        >
-          <div style={{ display: "grid", gridTemplateColumns: "minmax(0, 1fr) auto", columnGap: "16px", marginBottom: "8px" }}>
-            <strong style={{ minWidth: 0, overflowWrap: "break-word" }}>{t("QC_INSTALLATION_ASSET_SERIAL_NUMBER")}</strong>
-            <span style={{ whiteSpace: "nowrap", textAlign: "right" }}>{image?.serialNumber || "-"}</span>
-          </div>
-          <div style={{ display: "grid", gridTemplateColumns: "minmax(0, 1fr) auto", columnGap: "16px" }}>
-            <strong style={{ minWidth: 0, overflowWrap: "break-word" }}>{t("QC_INSTALLATION_ASSET_CAPACITY")}</strong>
-            <span style={{ whiteSpace: "nowrap", textAlign: "right" }}>{image?.capacity || "-"}</span>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
-};
 
 const Summary = ({
   t,
