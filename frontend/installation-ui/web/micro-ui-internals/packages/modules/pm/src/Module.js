6fc07cba24b85bccbbd9235d605a977042ae0384
revanth.korra <revanth.korra@beehyv.com>
2026-08-31 18:26:52 +0530

    fix:removed  amc modulecode

--- frontend/installation-ui/web/micro-ui-internals/packages/modules/pm/src/Module.js
+++ frontend/installation-ui/web/micro-ui-internals/packages/modules/pm/src/Module.js
@@ -20,7 +20,7 @@ import AssessmentActivityDetails from "./components/CreateAssessment/ActivityDet
 export const PMReducers = getRootReducer;
 
 const PMModule = ({ stateCode, userType, tenants }) => {
-  const moduleCode = ["PM", "AMC"];
+  const moduleCode = "PM";
   const language = Digit.StoreData.getCurrentLanguage();
   const { isLoading, data: store } = Digit.Services.useStore({ stateCode, moduleCode, language });
   const { info } = Digit.UserService.getUser();
