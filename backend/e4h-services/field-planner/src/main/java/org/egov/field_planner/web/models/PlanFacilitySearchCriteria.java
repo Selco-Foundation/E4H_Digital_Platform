125e8b4155bdf1b7775e67b71c6c199ba88e2499
Babacar Ndiaye <babacar.ndiay93@gmail.com>
2026-09-04 11:59:22 +0000

    Fix issue on field planner service

--- backend/e4h-services/field-planner/src/main/java/org/egov/field_planner/web/models/PlanFacilitySearchCriteria.java
+++ backend/e4h-services/field-planner/src/main/java/org/egov/field_planner/web/models/PlanFacilitySearchCriteria.java
@@ -1,7 +1,6 @@
 package org.egov.field_planner.web.models;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -14,8 +13,16 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class PlanFacilitySearchCriteria {
 
-    @JsonProperty("criteria")
+    @JsonProperty("planId")
     @NotNull
-    @Valid
-    private PlanFacilitySearchCriteria criteria;
+    private String planId;
+
+    @JsonProperty("filters")
+    private PlanFacilityFilters filters;
+
+    @JsonProperty("exportAll")
+    private Boolean exportAll;
+
+    @JsonProperty("includeResponseSummary")
+    private Boolean includeResponseSummary;
 }
