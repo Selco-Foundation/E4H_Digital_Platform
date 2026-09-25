8a50b63077f722e8f3be645eb8dcbcfae6e56e7e
jogapps <jogappsdeveloper@gmail.com>
2026-09-25 07:47:34 +0100

    updated compile sdk to 36 from 34 and min sdk to 24 from 23

--- mobile/android/app/build.gradle.kts
+++ mobile/android/app/build.gradle.kts
@@ -17,10 +17,11 @@ if (keystorePropertiesFile.exists()) {
 
 android {
     namespace = "org.e4h.asset"
-    compileSdk = 35
+    compileSdk = 36
     ndkVersion = "27.0.12077973"
 
     compileOptions {
+        isCoreLibraryDesugaringEnabled = true
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
@@ -31,8 +32,8 @@ android {
 
     defaultConfig {
         applicationId = "org.e4h.asset"
-        minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
+        minSdk = 24
+        targetSdk = 36
         versionCode = flutter.versionCode()
         versionName = flutter.versionName()
 
@@ -71,6 +72,10 @@ android {
             )
         }
     }
+}
+
+dependencies {
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.4")
 }
 
 flutter {
